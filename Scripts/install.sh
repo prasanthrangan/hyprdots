668fab3b0eeba1bdafbb53cb8f75468cd94a0121
Tittu <prasanthrangan@rediffmail.com>
2023-04-04 11:11:17 +0530

    initial restructuring

--- Scripts/install.sh
+++ Scripts/install.sh
@@ -6,16 +6,16 @@
 
 source global_fn.sh
 
-./install_pkg.sh custom_hypr.lst
 ./install_pkg.sh custom_pkg.lst
+./install_pkg.sh custom_hypr.lst
 ./install_pkg.sh custom_zsh.lst
-./install_pkg.sh custom_ext.lst
-
-./git_sync.sh
+#./install_pkg.sh custom_ext.lst
 
 ./install_fnt.sh
 ./install_cfg.sh
-./install_oth.sh
+#./install_oth.sh
 ./install_sgz.sh
 
-sudo ./enable_ctl.sh
+sudo ./enable_ctl.sh bluetooth
+sudo ./enable_ctl.sh sddm
+
--- Scripts/install_oth.sh
+++ Scripts/install_oth.sh
@@ -14,40 +14,6 @@ then
 fi
 
 
-# steam
-if pkg_installed steam
-then
-    if [ ! -d ~/.local/share/Steam/Skins/ ]
-    then
-        mkdir -p ~/.local/share/Steam/Skins/
-    fi
-    tar -xvzf ~/Dots/Source/arcs/Steam_Metro.tar.gz -C ~/.local/share/Steam/Skins/
-fi
-
-
-# spotify
-if pkg_installed spotify
-then
-    echo "launching spotify..."
-    /usr/bin/spotify &
-    spoty_pid=$!
-    sleep 5
-    kill -9 $spoty_pid
-
-    sudo chmod a+wr /opt/spotify
-    sudo chmod a+wr /opt/spotify/Apps -R
-
-    if [ $(ls -A ~/.config/spicetify/Backup | wc -l) -eq 0 ]
-    then
-        spicetify backup apply
-    fi
-
-    spicetify config current_theme Sleek
-    spicetify config color_scheme Cherry
-    spicetify apply
-fi
-
-
 # firefox
 if pkg_installed firefox
 then
