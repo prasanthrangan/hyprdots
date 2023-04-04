de3f1ee1d60baaa7f16104aa17d7219d823c0469
Tittu <106020512+prasanthrangan@users.noreply.github.com>
2023-04-04 17:35:55 +0530

    Update install_pkg.sh

--- Scripts/install_pkg.sh
+++ Scripts/install_pkg.sh
@@ -6,16 +6,27 @@
 
 source global_fn.sh
 
-install_list="${1:-essential_pkg.lst}"
-yay_chk=0
+install_list="${1:-custom_pkg.lst}"
+
+
+if pkg_installed git
+    then
+    echo "git dependency already installed..."
+else
+    sudo pacman -S git
+fi
+
+if pkg_installed yay
+    then
+    echo "yay dependency already installed..."
+else
+    ./install_yay.sh 2>&1
+fi
+
 
 while read pkg
 do
-    if [[ ${pkg} == "yay" ]]
-        then
-        yay_chk=1
-
-    elif pkg_installed ${pkg}
+    if pkg_installed ${pkg}
         then
         echo "skipping ${pkg}..."
 
@@ -34,19 +45,17 @@ do
     fi
 done < $install_list
 
+
 if [ `echo $pkg_arch | wc -w` -gt 0 ]
 then
     echo "installing $pkg_arch from arch repo..."
     sudo pacman -S $pkg_arch
 fi
 
-if [ $yay_chk -eq 1 ]
-then
-    ./install_yay.sh 2>&1
-fi
 
 if [ `echo $pkg_aur | wc -w` -gt 0 ]
 then
     echo "installing $pkg_aur from aur..."
     yay -S $pkg_aur
 fi
+
