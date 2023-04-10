8a7b5967a3c692808a644455acceb67089381f07
Tittu <prasanthrangan@rediffmail.com>
2023-04-10 23:19:36 +0530

    updates

--- Configs/.config/hypr/swwwallpaper.sh
+++ Configs/.config/hypr/swwwallpaper.sh
@@ -1,49 +1,59 @@
 #!/usr/bin/env sh
 
-if [ "$1" == "--help" ]
+Wall_Next()
+{
+    local WallNxt=$1
+    if [ -f $WallNxt ] && [ ! -z $WallNxt ]
+        then
+        Wallist=(`dirname $WallNxt`/*)
+
+        for((i=0;i<${#Wallist[@]};i++))
+        do
+            if [ $((i + 1)) -eq ${#Wallist[@]} ]
+                then
+                echo ${Wallist[0]} > $BASEDIR/wall.conf
+                Wall_Set ${Wallist[0]}
+                break
+            elif [ ${Wallist[i]} == ${WallNxt} ]
+                then
+                echo ${Wallist[i+1]} > $BASEDIR/wall.conf
+                Wall_Set ${Wallist[i+1]}
+                break   
+            fi
+        done
+    fi
+}
+
+Wall_Set()
+{
+    local WallSet=$1
+    if [ -f $WallSet ] && [ ! -z $WallSet ]
+        then
+        swww img $WallSet \
+        --transition-bezier .43,1.19,1,.4 \
+        --transition-type grow \
+        --transition-duration 1 \
+        --transition-fps 144 \
+        --transition-pos bottom-right &
+    fi
+}
+
+if [ "$1" == "--help" ] ;
     then
     echo ""
-    echo " Next Wallpaper"
+    echo "󰋫 Next Wallpaper 󰉼 󰆊"
 else
-
-swww query
-if [ $? -eq 1 ]
-    then
-    swww init
+    BASEDIR=$(dirname "$0")
+    touch $BASEDIR/wall.conf
+    Wall=`head -1 $BASEDIR/wall.conf`
+    swww query
+    if [ $? -eq 1 ]
+        then
+        swww init
+        sleep 3
+        Wall_Set $Wall
+    else
+        Wall_Next $Wall
+    fi
 fi
 
-WallDir="$HOME/Dots/Wallpapers"
-WallChk=`swww query | awk '{print $NF}' | sed 's/"//g'`
-
-if [ $WallChk == "000" ]
-
-    then
-    WallSet="${WallDir}/rain_world1.png"
-
-else
-
-    Wallist=(${WallDir}/*)
-    for((i=1;i<=${#Wallist[@]};i++))
-    do
-        if [ $i -eq ${#Wallist[@]} ]
-            then
-            WallSet=${Wallist[0]}
-            break
-
-        elif [ ${Wallist[i-1]} == ${WallDir}/${WallChk} ]
-            then
-            WallSet=${Wallist[i]}
-            break
-
-        fi
-    done
-
-fi
-
-swww img $WallSet \
---transition-type grow \
---transition-duration 2 \
---transition-fps 144 \
---transition-pos bottom-right
-
-fi
