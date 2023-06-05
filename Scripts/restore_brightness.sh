fa33e7535af79404f81e177f08d6030a6a301360
Yougraj <yougrajbora.developer@gmail.com>
2023-06-05 20:54:51 +0530

    brightness script adding

--- Scripts/restore_brightness.sh
+++ Scripts/restore_brightness.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
+
+if [ -d "$HOME/.local/bin" ] 
+then
+    echo "Directory /path/to/dir exists." 
+else
+    mkdir -p $HOME/.local/bin
+fi
+
+cp ../Configs/.local/bin/* $HOME/.local/bin
 sudo cp ~/.local/bin/changebrightness /usr/local/bin/ 
