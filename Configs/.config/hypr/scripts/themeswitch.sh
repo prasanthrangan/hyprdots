c2dada618a2d0c2834c93026008ea87df60a0428
tittu <prasanthrangan@rediffmail.com>
2023-06-10 17:37:38 +0530

    themeselect icon change and adding decay green

--- Configs/.config/hypr/scripts/themeswitch.sh
+++ Configs/.config/hypr/scripts/themeswitch.sh
@@ -93,6 +93,8 @@ killall -SIGUSR1 kitty
 
 ### qt5ct ###
 sed -i "/^color_scheme_path=/c\color_scheme_path=$ConfDir/qt5ct/colors/${ThemeSet}.conf" $ConfDir/qt5ct/qt5ct.conf
+IconSet=`awk -F "'" '$0 ~ /gsettings set org.gnome.desktop.interface icon-theme/{print $2}' $ConfDir/hypr/themes/${ThemeSet}.conf`
+sed -i "/^icon_theme=/c\icon_theme=${IconSet}" $ConfDir/qt5ct/qt5ct.conf
 
 
 ### rofi ###
