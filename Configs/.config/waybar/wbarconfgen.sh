4384ce6e3505d27878f3f70be2b691535d6630d6
tittu <prasanthrangan@rediffmail.com>
2023-06-24 16:07:00 +0530

    adding dynamic waybar config generator

--- Configs/.config/waybar/wbarconfgen.sh
+++ Configs/.config/waybar/wbarconfgen.sh
@@ -10,7 +10,7 @@ conf_ctl="$waybar_dir/config.ctl"
 
 readarray -t read_ctl < $conf_ctl
 num_files="${#read_ctl[@]}"
-nextIndex=0
+switch=0
 
 
 # update control file to set next/prev mode
@@ -21,17 +21,21 @@ do
 
     if [ $flag -eq 1 ] && [ "$1" == "n" ] ; then
         nextIndex=$(( (i + 1) % $num_files ))
+        switch=1
         break;
 
     elif [ $flag -eq 1 ] && [ "$1" == "p" ] ; then
         nextIndex=$(( i - 1 ))
+        switch=1
         break;
     fi
 done
 
-update_ctl="${read_ctl[nextIndex]}"
-sed -i "s/^1/0/g" $conf_ctl
-awk -F '|' -v thm="$update_ctl" '{OFS=FS} {if($0==thm) $1=1; print$0}' $conf_ctl > $waybar_dir/tmp && mv $waybar_dir/tmp $conf_ctl
+if [ $switch -eq 1 ] ; then
+    update_ctl="${read_ctl[nextIndex]}"
+    sed -i "s/^1/0/g" $conf_ctl
+    awk -F '|' -v cmp="$update_ctl" '{OFS=FS} {if($0==cmp) $1=1; print$0}' $conf_ctl > $waybar_dir/tmp && mv $waybar_dir/tmp $conf_ctl
+fi
 
 
 # module generator function
