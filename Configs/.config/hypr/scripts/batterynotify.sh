d0d8165cad3dbc94319f6e58365ea14ad44a988f
Mahaveer Gurjar <57872034+mahaveergurjar@users.noreply.github.com>
2023-09-01 20:23:50 +0530

    minor update

--- Configs/.config/hypr/scripts/batterynotify.sh
+++ Configs/.config/hypr/scripts/batterynotify.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
+# Check if the system is a laptop
+is_laptop() {
+    if [ -d /sys/class/power_supply/ ]; then
+        for supply in /sys/class/power_supply/*; do
+            if [ -e "$supply/type" ]; then
+                type=$(cat "$supply/type")
+                if [ "$type" == "Battery" ]; then
+                    return 0  # It's a laptop
+                fi
+            fi
+        done
+    fi
+    return 1  # It's not a laptop
+}
 
+if is_laptop; then
 while true; do
     battery_status=$(cat /sys/class/power_supply/BAT0/status)
     battery_percentage=$(cat /sys/class/power_supply/BAT0/capacity)
@@ -7,10 +22,21 @@ while true; do
     if [ "$battery_status" == "Discharging" ] && [ "$battery_percentage" -le 20 ]; then
         dunstify -u CRITICAL "Battery Low" "Battery is at $battery_percentage%. Connect the charger."
     fi
-
+    # Check if the laptop is plugged in and charging
+    if [ "$battey_status" == "Charging" ]; then
+            dunstify -u NORMAL "Charging: {$battery_status}%"
+    fi
+    
+    # battery charging notification
     if [ "$battery_status" == "Charging" ] && [ "$battery_percentage" -ge 80 ]; then
         dunstify -u NORMAL "Battery Charged" "Battery is at $battery_percentage%. You can unplug the charger."
     fi
 
-    sleep 300  # Sleep for 5 minutes before checking again
-done
+    # Check if the laptop is discharging
+    if [ "$_battery_status" == "Discharging" ]; then
+        # Notify when the battery level is below a 20%
+        if [ "$battery_percentage" -le 20 ]; then
+                dunstify -u CRITICAL "Battery Low" "Battery is at $battery_percentage%. Connect the charger."
+        fi
+    fi
+
