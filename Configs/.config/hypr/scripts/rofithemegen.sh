6cd249aea4952ad6b7ed4b2879f38f3a13b93b83
tittu <prasanthrangan@rediffmail.com>
2023-09-17 10:15:49 +0530

    adding wallbash toggle

--- Configs/.config/hypr/scripts/rofithemegen.sh
+++ Configs/.config/hypr/scripts/rofithemegen.sh
@@ -7,8 +7,13 @@ source $ScrDir/globalcontrol.sh
 dcoDir="$HOME/.config/hypr/dcols"
 rofThm="$HOME/.config/rofi/themes"
 
+
+# regen color conf
+
 if [ "$EnableWallDcol" -ne 1 ] ; then
     gtk_theme=`gsettings get org.gnome.desktop.interface gtk-theme | sed "s/'//g"`
-    cp ${rofThm}/${gtk_theme}.rasi ${rofThm}/Wall-Dcol.rasi
+    cp ${rofThm}/${gtk_theme}.rasi ${rofThm}/theme.rasi
+else
+    cp ${rofThm}/Wall-Dcol.rasi ${rofThm}/theme.rasi
 fi
 
