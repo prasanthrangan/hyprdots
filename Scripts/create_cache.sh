b342ce019987030d05e30ef202dd5a2a77fa3c6b
Trevin <132922589+T-Crypt@users.noreply.github.com>
2023-10-04 09:13:55 -0600

    Update create_cache.sh

--- Scripts/create_cache.sh
+++ Scripts/create_cache.sh
@@ -4,8 +4,6 @@
 #|-/ /--| Kemipso                            |-/ /--|#
 #|/ /---+------------------------------------+/ /---|#
 
-#!/bin/bash
-
 source global_fn.sh
 if [ $? -ne 0 ] ; then
     echo "Error: unable to source global_fn.sh, please execute from $(dirname $(realpath $0))..."
