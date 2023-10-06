1a74683d0519a817a41332f3b20612a86f2dedb8
stove-jeebs <1876595827qq@gmail.com>
2023-10-07 12:19:04 +1300

    add comments

--- Configs/.config/hypr/scripts/screenshot.sh
+++ Configs/.config/hypr/scripts/screenshot.sh
@@ -12,11 +12,15 @@ save_file=$(date +'%y%m%d_%Hh%Mm%Ss_screenshot.png')
 mkdir -p $save_dir
 
 case $1 in
+# print all monitors
 p)  grim $save_dir/$save_file ;;
+# screenshot snip
 s)  mkdir -p $swpy_dir
     echo -e "[Default]\nsave_dir=$save_dir\nsave_filename_format=$save_file" > $swpy_dir/config
     grim -g "$(slurp)" - | swappy -f - ;;
+# print focused monitor
 m)  grim -o "$(hyprctl -j activeworkspace | jq -r '.monitor')" - | swappy -f - ;;
+# print focused window
 w)  grim -g "$(hyprctl -j activewindow | jq -j '"\(.at[0]),\(.at[1]) \(.size[0])x\(.size[1])"')" \
       - | swappy -f - ;;
 *)  echo "...valid options are..."
