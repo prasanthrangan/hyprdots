decf7d2af1f43e0c4236de1a65ada0e9ce9d4116
stove-jeebs <1876595827qq@gmail.com>
2023-10-07 12:13:15 +1300

    print current focused monitor or window

--- Configs/.config/hypr/scripts/screenshot.sh
+++ Configs/.config/hypr/scripts/screenshot.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
 if [ -z "$XDG_PICTURES_DIR" ] ; then
-    XDG_PICTURES_DIR="$HOME/Pictures"
+    XDG_PICTURES_DIR="$HOME/Pictures/screenshots/"
 fi
 
 ScrDir=`dirname $(realpath $0)`
@@ -16,6 +16,9 @@ p)  grim $save_dir/$save_file ;;
 s)  mkdir -p $swpy_dir
     echo -e "[Default]\nsave_dir=$save_dir\nsave_filename_format=$save_file" > $swpy_dir/config
     grim -g "$(slurp)" - | swappy -f - ;;
+m)  grim -o "$(hyprctl -j activeworkspace | jq -r '.monitor')" - | swappy -f - ;;
+w)  grim -g "$(hyprctl -j activewindow | jq -j '"\(.at[0]),\(.at[1]) \(.size[0])x\(.size[1])"')" \
+      - | swappy -f - ;;
 *)  echo "...valid options are..."
     echo "p : print screen to $save_dir"
     echo "s : snip current screen to $save_dir"   
