0b2c3375d2bf14577365252517b4208351ef14f7
stove-jeebs <1876595827qq@gmail.com>
2023-10-11 09:11:23 +1300

    use grimblast for screenshots

--- Configs/.config/hypr/scripts/screenshot.sh
+++ Configs/.config/hypr/scripts/screenshot.sh
@@ -12,17 +12,14 @@ save_file=$(date +'%y%m%d_%Hh%Mm%Ss_screenshot.png')
 mkdir -p $save_dir
 
 case $1 in
-# print all monitors
-p)  grim $save_dir/$save_file ;;
+# print all outputs
+p)  grimblast save screen - | swappy -f - ;;
 # screenshot snip
 s)  mkdir -p $swpy_dir
     echo -e "[Default]\nsave_dir=$save_dir\nsave_filename_format=$save_file" > $swpy_dir/config
-    grim -g "$(slurp)" - | swappy -f - ;;
+    grimblast --freeze save area - | swappy -f - ;;
 # print focused monitor
-m)  grim -o "$(hyprctl -j activeworkspace | jq -r '.monitor')" - | swappy -f - ;;
-# print focused window
-w)  grim -g "$(hyprctl -j activewindow | jq -j '"\(.at[0]),\(.at[1]) \(.size[0])x\(.size[1])"')" \
-      - | swappy -f - ;;
+m)  grimblast save output - | swappy -f - ;;
 *)  echo "...valid options are..."
     echo "p : print screen to $save_dir"
     echo "s : snip current screen to $save_dir"   
