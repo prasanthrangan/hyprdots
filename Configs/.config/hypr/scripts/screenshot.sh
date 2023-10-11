392f05e633958a4b5855b6ea9cdbd04dfd6b8ed8
stove-jeebs <1876595827qq@gmail.com>
2023-10-11 20:43:23 +1300

    use copysave for all screenshots

--- Configs/.config/hypr/scripts/screenshot.sh
+++ Configs/.config/hypr/scripts/screenshot.sh
@@ -11,20 +11,23 @@ save_dir="${2:-$XDG_PICTURES_DIR}"
 save_file=$(date +'%y%m%d_%Hh%Mm%Ss_screenshot.png')
 mkdir -p $save_dir
 
+temp_screenshot="/tmp/screenshot.png"
 case $1 in
 # print all outputs
-p)  grimblast save screen - | swappy -f - ;;
+p)  grimblast copysave screen $temp_screenshot && swappy -f $temp_screenshot ;;
 # drag to manually snip an area / click on a window to print it
 s)  mkdir -p $swpy_dir
     echo -e "[Default]\nsave_dir=$save_dir\nsave_filename_format=$save_file" > $swpy_dir/config
-    grimblast --freeze save area - | swappy -f - ;;
+    grimblast  --freeze copysave area $temp_screenshot && swappy -f $temp_screenshot ;;
 # print focused monitor
-m)  grimblast save output - | swappy -f - ;;
+m)  grimblast copysave output $temp_screenshot && swappy -f $temp_screenshot ;;
 *)  echo "...valid options are..."
     echo "p : print screen to $save_dir"
     echo "s : snip current screen to $save_dir"   
     exit 1 ;;
 esac
+rm "$temp_screenshot"
+
 
 if [ -f "$save_dir/$save_file" ] ; then
     dunstify $ncolor "theme" -a "saved in $save_dir" -i "$save_dir/$save_file" -r 91190 -t 2200
