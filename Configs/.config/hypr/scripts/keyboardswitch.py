a426eb046c398b0b5f27671a15738c2e1cdc67ad
Trevin <132922589+T-Crypt@users.noreply.github.com>
2023-10-11 12:56:23 -0600

    added keyboardswitch.py

---
+++ Configs/.config/hypr/scripts/keyboardswitch.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+
+import json
+import subprocess
+import sys
+ 
+def get_keyboards():
+    output = subprocess.check_output(["hyprctl","devices","-j"]).decode("utf-8")
+    if output:
+        devices=json.loads(output)
+    return devices["keyboards"]
+ 
+def change_layout():
+    active_keymap=False
+    for keyboard in get_keyboards():
+        if active_keymap and active_keymap != keyboard["active_keymap"]:
+            print ("The keyboard %s has the layout %s which differs from %s, equalizing"%(keyboard["name"],keyboard["active_keymap"],active_keymap))
+            return False #Not every keyboard is at the same layout currently
+        active_keymap=keyboard["active_keymap"]
+        command = ["hyprctl","switchxkblayout",keyboard["name"],"next"]
+        result = subprocess.run(command,stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+        if result.returncode == 0:
+            print("Switched the layout for %s.\n%s" % (keyboard["name"],result.stdout))
+        else:
+            sys.stderr.write("Error while switching the layout for %s.\n%s\n" % (keyboard["name"],result.stderr))
+            got_error = True
+    return True #All layouts had been cycled successfully
+ 
+got_error = False
+while not change_layout():
+    pass
+if got_error:
+    sys.stderr.write("Some errors were found during the process...\n")
+    sys.exit(1)
