f393da0ae363bc72f1eea03e705c8ab51d21b1a7
tittu <prasanthrangan@rediffmail.com>
2023-10-15 01:53:31 +0530

    minor updates

--- Configs/.config/hypr/scripts/keyboardswitch.sh
+++ Configs/.config/hypr/scripts/keyboardswitch.sh
@@ -8,7 +8,6 @@ do
     hyprctl switchxkblayout "$keyName" next
 done
 
-keyMain=$(hyprctl -j devices | jq '.keyboards' | jq '.[] | select (.main == true)' | awk -F '"' '{if ($2=="name") print $4}')
 layMain=$(hyprctl -j devices | jq '.keyboards' | jq '.[] | select (.main == true)' | awk -F '"' '{if ($2=="active_keymap") print $4}')
+dunstify "t1" -i ~/.config/dunst/icons/keyboard.svg -a "$layMain" -r 91190 -t 800
 
-dunstify "t1" -i ~/.config/dunst/icons/keyboard.svg -a "$layMain" "$keyMain" -r 91190 -t 800
