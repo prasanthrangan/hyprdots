d4c4bb23c2721e974a1d59a525a7ebe2fd2a8ef1
stove-jeebs <1876595827qq@gmail.com>
2023-10-15 07:40:09 +1300

    move swappy configuration out of case statement

--- Configs/.config/hypr/scripts/screenshot.sh
+++ Configs/.config/hypr/scripts/screenshot.sh
@@ -7,23 +7,24 @@ fi
 ScrDir=`dirname $(realpath $0)`
 source $ScrDir/globalcontrol.sh
 swpy_dir="$HOME/.config/swappy"
-save_dir="${2:-$XDG_PICTURES_DIR/Screenshots}"
+save_dir="${2:-$XDG_PICTURES_DIR/Screenshots/}"
 save_file=$(date +'%y%m%d_%Hh%Mm%Ss_screenshot.png')
 mkdir -p $save_dir
+mkdir -p $swpy_dir
+echo -e "[Default]\nsave_dir=$save_dir\nsave_filename_format=$save_file" > $swpy_dir/config
 
 temp_screenshot="/tmp/screenshot.png"
 case $1 in
 # print all outputs
 p)  grimblast copysave screen $temp_screenshot && swappy -f $temp_screenshot ;;
 # drag to manually snip an area / click on a window to print it
-s)  mkdir -p $swpy_dir
-    echo -e "[Default]\nsave_dir=$save_dir\nsave_filename_format=$save_file" > $swpy_dir/config
-    grimblast  --freeze copysave area $temp_screenshot && swappy -f $temp_screenshot ;;
+s)  grimblast  --freeze copysave area $temp_screenshot && swappy -f $temp_screenshot ;;
 # print focused monitor
 m)  grimblast copysave output $temp_screenshot && swappy -f $temp_screenshot ;;
 *)  echo "...valid options are..."
     echo "p : print screen to $save_dir"
-    echo "s : snip current screen to $save_dir"   
+    echo "s : snip current screen or focused window to $save_dir"   
+    echo "m : print current monitor to $save_dir"   
     exit 1 ;;
 esac
 rm "$temp_screenshot"
