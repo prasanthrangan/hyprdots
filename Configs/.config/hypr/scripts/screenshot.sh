0a7b0c3bc7681dde8d242833de2fba4b8bfa5dfd
tittu <prasanthrangan@rediffmail.com>
2023-10-15 10:57:36 +0530

    screenshot actions fix #214

--- Configs/.config/hypr/scripts/screenshot.sh
+++ Configs/.config/hypr/scripts/screenshot.sh
@@ -7,8 +7,9 @@ fi
 ScrDir=`dirname $(realpath $0)`
 source $ScrDir/globalcontrol.sh
 swpy_dir="$HOME/.config/swappy"
-save_dir="${2:-$XDG_PICTURES_DIR}"
+save_dir="${2:-$XDG_PICTURES_DIR/Screenshots}"
 save_file=$(date +'%y%m%d_%Hh%Mm%Ss_screenshot.png')
+temp_screenshot="/tmp/screenshot.png"
 
 mkdir -p $save_dir
 mkdir -p $swpy_dir
@@ -19,16 +20,24 @@ function print_error
 cat << "EOF"
     ./screenshot.sh <action>
     ...valid actions are...
-        p : print current screen
+        p : print all screens
         s : snip current screen
+        m : print focused monitor
 EOF
 }
 
 case $1 in
-p)  grim $save_dir/$save_file ;;
-s)  grim -g "$(slurp)" - | swappy -f - ;;
-*)  print_error ;;
+p)  # print all outputs
+    grimblast copysave screen $temp_screenshot && swappy -f $temp_screenshot ;;
+s)  # drag to manually snip an area / click on a window to print it
+    grimblast --freeze copysave area $temp_screenshot && swappy -f $temp_screenshot ;;
+m)  # print focused monitor
+    grimblast copysave output $temp_screenshot && swappy -f $temp_screenshot ;;
+*)  # invalid option
+    print_error ;;
 esac
+
+rm "$temp_screenshot"
 
 if [ -f "$save_dir/$save_file" ] ; then
     dunstify "t1" -a "saved in $save_dir" -i "$save_dir/$save_file" -r 91190 -t 2200
