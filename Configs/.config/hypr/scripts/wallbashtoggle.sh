b64e2819f59987562388473a7e7962cf2cf40589
tittu <prasanthrangan@rediffmail.com>
2023-11-01 18:42:17 +0530

    performance enhancement and temp fix #409

--- Configs/.config/hypr/scripts/wallbashtoggle.sh
+++ Configs/.config/hypr/scripts/wallbashtoggle.sh
@@ -16,7 +16,7 @@ else
 fi
 
 # reset the colors
-grep -m 1 '.' $DcoDir/*.dcol | cut -d '|' -f 2 | while read wallbash
+grep -m 1 '.' $DcoDir/*.dcol | cut -d '|' -f 3 | while read wallbash
 do
     if [ ! -z "$wallbash" ] ; then
         sh -c "ScrDir=`dirname $(realpath $0)` && $wallbash"
