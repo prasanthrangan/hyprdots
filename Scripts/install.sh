ad69698adeff0ecbe92179723953a5cc45465d96
tittu <prasanthrangan@rediffmail.com>
2023-12-02 21:21:07 +0530

    preserve keybindings.conf updated by user

--- Scripts/install.sh
+++ Scripts/install.sh
@@ -85,13 +85,10 @@ EOF
     # add nvidia drivers to the list #
     #--------------------------------#
     if nvidia_detect; then
-
         cat /usr/lib/modules/*/pkgbase | while read krnl; do
             echo "${krnl}-headers" >>install_pkg.lst
         done
-
         echo -e "nvidia-dkms\nnvidia-utils" >>install_pkg.lst
-
     else
         echo "nvidia card not detected, skipping nvidia drivers..."
     fi
