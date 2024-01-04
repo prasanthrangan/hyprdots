011e440db7e9b69a94f071c461c6688beac79ccd
joshrandall8478 <joshrandall8478@gmail.com>
2024-01-04 14:31:41 -0500

    Moved location of code scripts to .config

--- Configs/scripts/code_restore_custom_bar.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env sh    
-set -eu    
-     
-settings_path="/home/$USER/.config/Code/User/settings.json"
-style="custom"  
-cat <<< $(jq ".\"window.titleBarStyle\" = \"$style\"" $settings_path) > $settings_path    
-
-notify-send -a "Visual Studio Code" -i /usr/share/icons/visual-studio-code.png "Custom title bar has been restored."
--- Configs/scripts/code_restore_native_bar.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env sh    
-set -eu    
-     
-settings_path="/home/$USER/.config/Code/User/settings.json"
-style="native"  
-cat <<< $(jq ".\"window.titleBarStyle\" = \"$style\"" $settings_path) > $settings_path    
-
-notify-send -a "Visual Studio Code" -i /usr/share/icons/visual-studio-code.png "Native title bar has been restored."
