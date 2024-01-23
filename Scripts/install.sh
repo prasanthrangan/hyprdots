452aba507784b00b38408d689249c8948a82cfe8
dewaltz <124180983+dewaltz@users.noreply.github.com>
2024-01-23 04:22:08 +0000

    Update install.sh
    updated nivida

--- Scripts/install.sh
+++ Scripts/install.sh
@@ -120,20 +120,6 @@ EOF
     #--------------------------------#
     # add nvidia drivers to the list #
     #--------------------------------#
-    if nvidia_detect; then
-        cat /usr/lib/modules/*/pkgbase | while read krnl; do
-            echo "${krnl}-headers" >>install_pkg.lst
-        done
-        IFS=$' ' read -r -d '' -a nvga < <(lspci -k | grep -E "(VGA|3D)" | grep -i nvidia | awk -F ':' '{print $NF}' | tr -d '[]()' && printf '\0')
-        for nvcode in "${nvga[@]}"; do
-            awk -F '|' -v nvc="${nvcode}" '{if ($3 == nvc) {split(FILENAME,driver,"/"); print driver[length(driver)],"\nnvidia-utils"}}' .nvidia/nvidia*dkms >>install_pkg.lst
-        done
-        echo -e "\033[0;32m[GPU]\033[0m: detected // ${nvga[@]}"
-    else
-        echo "nvidia card not detected, skipping nvidia drivers..."
-    fi
-
-#########################################################
 
         nvidia_pkg=(
           nvidia-dkms
@@ -209,7 +195,6 @@ EOF
         else
           printf "${NOTE} Skipping nouveau blacklisting.\n" 2>&1 | tee -a "$LOG"
         fi
-
 
 
     #--------------------------------#
