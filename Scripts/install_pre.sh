37b935190ae94bc7fbd43731d0da380c64f5f467
dewaltz <124180983+dewaltz@users.noreply.github.com>
2024-01-23 04:34:24 +0000

    Update install_pre.sh
    removed grub config

--- Scripts/install_pre.sh
+++ Scripts/install_pre.sh
@@ -10,47 +10,6 @@ if [ $? -ne 0 ] ; then
     exit 1
 fi
 
-
-# grub
-if pkg_installed grub && [ -f /boot/grub/grub.cfg ]
-    then
-    echo -e "\033[0;32m[BOOTLOADER]\033[0m: grub detected..."
-
-    if [ ! -f /etc/default/grub.t2.bkp ] && [ ! -f /boot/grub/grub.t2.bkp ]
-        then
-        echo -e "\033[0;32m[BOOTLOADER]\033[0m: configuring grub..."
-        sudo cp /etc/default/grub /etc/default/grub.t2.bkp
-        sudo cp /boot/grub/grub.cfg /boot/grub/grub.t2.bkp
-
-        if nvidia_detect
-            then
-            echo -e "\033[0;32m[BOOTLOADER]\033[0m: nvidia detected, adding nvidia_drm.modeset=1 to boot option..."
-            gcld=$(grep "^GRUB_CMDLINE_LINUX_DEFAULT=" "/etc/default/grub" | cut -d'"' -f2 | sed 's/\b nvidia_drm.modeset=.\b//g')
-            sudo sed -i "/^GRUB_CMDLINE_LINUX_DEFAULT=/c\GRUB_CMDLINE_LINUX_DEFAULT=\"${gcld} nvidia_drm.modeset=1\"" /etc/default/grub
-        fi
-
-        read -p "Apply grub theme? [Y/N] : " grubtheme
-        case $grubtheme in
-        Y|y) echo -e "\033[0;32m[BOOTLOADER]\033[0m: Setting grub theme..."
-            sudo tar -xzf ${CloneDir}/Source/arcs/Grub_Pochita.tar.gz -C /usr/share/grub/themes/
-            sudo sed -i "/^GRUB_DEFAULT=/c\GRUB_DEFAULT=saved
-            /^GRUB_GFXMODE=/c\GRUB_GFXMODE=1280x1024x32,auto
-            /^#GRUB_THEME=/c\GRUB_THEME=\"/usr/share/grub/themes/pochita/theme.txt\"
-            /^#GRUB_SAVEDEFAULT=true/c\GRUB_SAVEDEFAULT=true" /etc/default/grub ;;
-        *) echo -e "\033[0;32m[BOOTLOADER]\033[0m: Skippinng grub theme..." 
-            sudo sed -i "s/^GRUB_THEME=/#GRUB_THEME=/g" /etc/default/grub ;;
-        esac
-
-        sudo grub-mkconfig -o /boot/grub/grub.cfg
-    else
-        echo -e "\033[0;32m[BOOTLOADER]\033[0m: grub is already configured..."
-    fi
-
-else
-    echo -e "\033[0;33m[WARNING]\033[0m: grub is not configured..."
-fi
-
-
 # systemd-boot
 if pkg_installed systemd && nvidia_detect && [ $(bootctl status | awk '{if ($1 == "Product:") print $2}') == "systemd-boot" ]
     then
