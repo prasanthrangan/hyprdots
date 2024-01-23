50da2b36a3d6ffa375270b8126539c0b7a4f79d8
dewaltz <124180983+dewaltz@users.noreply.github.com>
2024-01-23 05:57:33 +0000

    Update install.sh
    removed  | tee -a "$LOG"

--- Scripts/install.sh
+++ Scripts/install.sh
@@ -132,19 +132,19 @@ EOF
         # Install additional Nvidia packages
         for krnl in $(cat /usr/lib/modules/*/pkgbase); do
           for NVIDIA in "${krnl}-headers" "${nvidia_pkg[@]}"; do
-            install_package "$NVIDIA" 2>&1 | tee -a "$LOG"
+            install_package "$NVIDIA"
           done
         done
         
         # Check if the Nvidia modules are already added in mkinitcpio.conf and add if not
         if grep -qE '^MODULES=.*nvidia. *nvidia_modeset.*nvidia_uvm.*nvidia_drm' /etc/mkinitcpio.conf; then
-          echo "Nvidia modules already included in /etc/mkinitcpio.conf" 2>&1 | tee -a "$LOG"
+          echo "Nvidia modules already included in /etc/mkinitcpio.conf" 
         else
-          sudo sed -Ei 's/^(MODULES=\([^\)]*)\)/\1 nvidia nvidia_modeset nvidia_uvm nvidia_drm)/' /etc/mkinitcpio.conf 2>&1 | tee -a "$LOG"
+          sudo sed -Ei 's/^(MODULES=\([^\)]*)\)/\1 nvidia nvidia_modeset nvidia_uvm nvidia_drm)/' /etc/mkinitcpio.conf
           echo "Nvidia modules added in /etc/mkinitcpio.conf"
         fi
         
-        sudo mkinitcpio -P 2>&1 | tee -a "$LOG"
+        sudo mkinitcpio -P
         printf "\n\n\n"
         
         # Additional Nvidia steps
@@ -155,7 +155,7 @@ EOF
         else
           printf "\n"
           printf "${YELLOW} Adding options to $NVEA..."
-          sudo echo -e "options nvidia-drm modeset=1" | sudo tee -a /etc/modprobe.d/nvidia.conf 2>&1 | tee -a "$LOG"
+          sudo echo -e "options nvidia-drm modeset=1" | sudo tee -a /etc/modprobe.d/nvidia.conf
           printf "\n"
         fi
         
@@ -170,19 +170,19 @@ EOF
             printf "${OK} Seems like nouveau is already blacklisted..moving on.\n"
           else
             printf "\n"
-            echo "blacklist nouveau" | sudo tee -a "$NOUVEAU" 2>&1 | tee -a "$LOG"
+            echo "blacklist nouveau" | sudo tee -a "$NOUVEAU"
             printf "${NOTE} has been added to $NOUVEAU.\n"
             printf "\n"
         
             # To completely blacklist nouveau (See wiki.archlinux.org/title/Kernel_module#Blacklisting 6.1)
             if [ -f "/etc/modprobe.d/blacklist.conf" ]; then
-              echo "install nouveau /bin/true" | sudo tee -a "/etc/modprobe.d/blacklist.conf" 2>&1 | tee -a "$LOG"
+              echo "install nouveau /bin/true" | sudo tee -a "/etc/modprobe.d/blacklist.conf"
             else
-              echo "install nouveau /bin/true" | sudo tee "/etc/modprobe.d/blacklist.conf" 2>&1 | tee -a "$LOG"
+              echo "install nouveau /bin/true" | sudo tee "/etc/modprobe.d/blacklist.conf"
             fi
           fi
         else
-          printf "${NOTE} Skipping nouveau blacklisting.\n" 2>&1 | tee -a "$LOG"
+          printf "${NOTE} Skipping nouveau blacklisting.\n"
         fi
 
 
