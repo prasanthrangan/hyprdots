63ceb9e8e330c01038978620cbaf1385b5b68717
5ouls3dge <124180983+5ouls3dge@users.noreply.github.com>
2024-01-25 21:36:05 +0000

    Update install_pre.sh

--- Scripts/install_pre.sh
+++ Scripts/install_pre.sh
@@ -22,13 +22,6 @@ if pkg_installed grub && [ -f /boot/grub/grub.cfg ]
         sudo cp /etc/default/grub /etc/default/grub.t2.bkp
         sudo cp /boot/grub/grub.cfg /boot/grub/grub.t2.bkp
 
-        if nvidia_detect
-            then
-            echo -e "\033[0;32m[BOOTLOADER]\033[0m: nvidia detected, adding nvidia_drm.modeset=1 to boot option..."
-            gcld=$(grep "^GRUB_CMDLINE_LINUX_DEFAULT=" "/etc/default/grub" | cut -d'"' -f2 | sed 's/\b nvidia_drm.modeset=.\b//g')
-            sudo sed -i "/^GRUB_CMDLINE_LINUX_DEFAULT=/c\GRUB_CMDLINE_LINUX_DEFAULT=\"${gcld} nvidia_drm.modeset=1\"" /etc/default/grub
-        fi
-
         read -p "Apply grub theme? [Y/N] : " grubtheme
         case $grubtheme in
         Y|y) echo -e "\033[0;32m[BOOTLOADER]\033[0m: Setting grub theme..."
@@ -49,30 +42,6 @@ if pkg_installed grub && [ -f /boot/grub/grub.cfg ]
 else
     echo -e "\033[0;33m[WARNING]\033[0m: grub is not configured..."
 fi
-
-
-# systemd-boot
-if pkg_installed systemd && nvidia_detect && [ $(bootctl status | awk '{if ($1 == "Product:") print $2}') == "systemd-boot" ]
-    then
-    echo -e "\033[0;32m[BOOTLOADER]\033[0m: systemd-boot detected..."
-
-    if [ $(ls -l /boot/loader/entries/*.conf.t2.bkp 2> /dev/null | wc -l) -ne $(ls -l /boot/loader/entries/*.conf 2> /dev/null | wc -l) ]
-        then
-        echo "nvidia detected, adding nvidia_drm.modeset=1 to boot option..."
-        find /boot/loader/entries/ -type f -name "*.conf" | while read imgconf
-        do
-            sudo cp ${imgconf} ${imgconf}.t2.bkp
-            sdopt=$(grep -w "^options" ${imgconf} | sed 's/\b quiet\b//g' | sed 's/\b splash\b//g' | sed 's/\b nvidia_drm.modeset=.\b//g')
-            sudo sed -i "/^options/c${sdopt} quiet splash nvidia_drm.modeset=1" ${imgconf}
-        done
-    else
-        echo -e "\033[0;33m[SKIP]\033[0m: systemd-boot is already configured..."
-    fi
-
-else
-    echo -e "\033[0;33m[WARNING]\033[0m: systemd-boot is not configured..."
-fi
-
 
 # pacman
 if [ -f /etc/pacman.conf ] && [ ! -f /etc/pacman.conf.t2.bkp ]
