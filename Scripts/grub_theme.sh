dcc7d229f22d8a9f8f6c9192fb6936383d5889f0
5ouls3dge <124180983+5ouls3dge@users.noreply.github.com>
2024-01-26 03:45:31 +0000

    Update grub_theme.sh

--- Scripts/grub_theme.sh
+++ Scripts/grub_theme.sh
@@ -1,3 +1,15 @@
+#!/bin/bash
+#|---/ /+-------------------------------------+---/ /|#
+#|--/ /-| Script to apply pre install configs |--/ /-|#
+#|-/ /--| Prasanth Rangan                     |-/ /--|#
+#|/ /---+-------------------------------------+/ /---|#
+
+source global_fn.sh
+if [ $? -ne 0 ] ; then
+    echo "Error: unable to source global_fn.sh, please execute from $(dirname "$(realpath "$0")")..."
+    exit 1
+fi
+
 if pkg_installed grub && [ -f /boot/grub/grub.cfg ]
 then
     echo -e "\033[0;32m[BOOTLOADER]\033[0m: grub detected..."
