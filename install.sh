588a845892720e5264ac3230a24f587cf27b3df9
5ouls3dge <124180983+5ouls3dge@users.noreply.github.com>
2024-01-27 03:42:19 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,32 +5,65 @@
 # by 5ouls3dge (2024)                      #
 #                                          #
 ############################################
+#!/bin/bash
 
 PS3="Select an option: "
 
-select main_option in "Install HyprWorld" "Install Arch Only" "Install Hyprland only" "Run Nvidia Setup" "Run Plymouth Install" "Quit"
+select main_option in "Installation Options" "Run Scripts" "Create HyprWorld" "Quit"
 do
     case $main_option in
-        "Install HyprWorld")
-            echo "Installing HyprWorld..."
-            ./arch/install.sh
-            ./hyprland/install.sh
+        "Installation Options")
+            echo "Select an installation option:"
+            select install_option in "Install HyprWorld" "Install Arch Only" "Install Hyprland only" "Back to Main Menu"
+            do
+                case $install_option in
+                    "Install HyprWorld")
+                        echo "Installing HyprWorld..."
+                        ./arch/install.sh
+                        ./hyprland/install.sh
+                        ;;
+                    "Install Arch Only")
+                        echo "Installing Arch Only..."
+                        ./arch/install.sh
+                        ;;
+                    "Install Hyprland only")
+                        echo "Installing Hyprland only..."
+                        ./hyprland/install.sh
+                        ;;
+                    "Back to Main Menu")
+                        break
+                        ;;
+                    *)
+                        echo "Invalid option. Please select a valid option."
+                        ;;
+                esac
+            done
             ;;
-        "Install Arch Only")
-            echo "Installing Arch Only..."
-            ./arch/install.sh
+        "Run Scripts")
+            echo "Select a script to run:"
+            select script_option in "Run Nvidia Setup" "Run Plymouth Install" "Back to Main Menu"
+            do
+                case $script_option in
+                    "Run Nvidia Setup")
+                        echo "Running Nvidia Setup..."
+                        ./Config/nvidia.sh
+                        ;;
+                    "Run Plymouth Install")
+                        echo "Running Plymouth Install..."
+                        ./Config/plymouth.sh
+                        ;;
+                    "Back to Main Menu")
+                        break
+                        ;;
+                    *)
+                        echo "Invalid option. Please select a valid option."
+                        ;;
+                esac
+            done
             ;;
-        "Install Hyprland only")
-            echo "Installing Hyprland only..."
-            ./hyprland/install.sh
-            ;;
-        "Run Nvidia Setup")
-            echo "Running Nvidia Setup..."
-            ./Config/nvidia.sh
-            ;;
-        "Run Plymouth Install")
-            echo "Running Plymouth Install..."
-            ./Config/plymouth.sh
+        "Create HyprWorld")
+            echo "Creating HyprWorld..."
+            # Add logic to create HyprWorld
             ;;
         "Quit")
             echo "Exiting..."
@@ -41,5 +74,6 @@ do
             ;;
     esac
 done
+
 
 # End of script
