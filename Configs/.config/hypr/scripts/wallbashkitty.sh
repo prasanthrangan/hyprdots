6a466b30d7b47db3d118a399d63d710391d374a3
khing-_- <alampayk@gmail.com>
2024-01-31 13:39:17 +0800

    Update wallbashkitty.sh

--- Configs/.config/hypr/scripts/wallbashkitty.sh
+++ Configs/.config/hypr/scripts/wallbashkitty.sh
@@ -5,11 +5,16 @@ ScrDir=`dirname "$(realpath "$0")"`
 source $ScrDir/globalcontrol.sh
 kittyDir="${XDG_CONFIG_HOME:-${HOME}/.config}/kitty"
 
+if ! grep -q 'include themes/Wall-Dcol.conf' "${kittyDir}/kitty.conf"; then
+    echo "include themes/Wall-Dcol.conf" >> "${kittyDir}/kitty.conf"
+fi
+
 #Override kitty config
 if [ "${EnableWallDcol}" -ne 1 ] ; then
  sed -i '/include themes\/Wall-Dcol\.conf/ s/^/#/' "${kittyDir}/kitty.conf"
 else
- sed -i '/include themes\/Wall-Dcol\.conf/ s/^#//' "${kittyDir}/kitty.conf"
+#  sed -i '/include themes\/Wall-Dcol\.conf/ s/^#//' "${kittyDir}/kitty.conf"
+ sed -i '/include themes\/Wall-Dcol\.conf/ s/^\s*##*//' "${kittyDir}/kitty.conf"
 fi
 
 killall -SIGUSR1 kitty
