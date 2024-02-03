db5f094e2a6aa94801e6de22fcc243a168da5e12
Khing <53417443+kRHYME7@users.noreply.github.com>
2024-02-03 15:22:54 +0800

    [READY] GPU info Script  (#643)
    * Update gpuinfo.sh
    
    * Moved The Icon to the Script
    
    * Moved The Icon to the Script
    
    * Update gpuinfo.sh
    
    Update gpuinfo.sh
    
    * Reverting some changes
    
    * Update gpuinfo.jsonc
    
    * Update gpuinfo.sh
    
    Update gpuinfo.sh
    
    Update gpuinfo.jsonc
    
    @Skerse  : Remove decimals
    
    Update gpuinfo.sh
    
    Update gpuinfo.sh
    
    Update gpuinfo.sh
    
    Update gpuinfo.sh
    
    Update gpuinfo.sh
    
    Update gpuinfo.sh
    
    Update gpuinfo.jsonc
    
    Update gpuinfo.jsonc
    
    Toggle option to change GPU for Multi GPU
    
    Update gpuinfo.jsonc
    
    Update gpuinfo.sh
    
    nouveau drivers support for gen query
    
    * Clean up + @KAGEYAMA Feature Request
    
    * Clean up + @KAGEYAMA Feature Request
    
    Update gpuinfo.sh
    
    Update gpuinfo.sh
    
    Update gpuinfo.jsonc
    
    @Skerse  : Remove decimals
    
    Update gpuinfo.sh
    
    Update gpuinfo.sh
    
    Update gpuinfo.sh
    
    Update gpuinfo.sh
    
    Update gpuinfo.sh
    
    Update gpuinfo.sh
    
    Update gpuinfo.jsonc
    
    Update gpuinfo.jsonc
    
    Toggle option to change GPU for Multi GPU
    
    Update gpuinfo.jsonc
    
    Update gpuinfo.sh
    
    nouveau drivers support for gen query
    
    * Proper Suspend Output
    
    * Startup flag
    
    * Fix_warning
    
    * Added tired flag to  not use query nvidia if suspend
    
    Update gpuinfo.jsonc
    
    Added single gpuinfo waybar module for multiple gpu
    
    Just a template so that users can just modify there waybar's config.ctl to add like this
    custom/gpuinfo#nvidia etc
    
    nvidia-smi cmd check
    
    syntxt fx
    
    Tooltip: Display GPU model
    
    Remove Displaying of GPU model for AMD and INTEL
    
    Update gpuinfo.sh
    
    Update gpuinfo.sh
    
    Bugfx
    
    Update gpuinfo.sh
    
    Added Help plus Comply with Shellcheck
    
    Update gpuinfo.sh
    
    Update gpuinfo.sh
    
    Remove Displaying of GPU model for AMD and INTEL
    
    I only now have a single device so  I cannot test the output and may cause errors.
    
    * Adding * for suspend or suspended
    
    ---------
    
    Co-authored-by: $VDFOREVER <55760436+VDFOREVER@users.noreply.github.com>

--- Configs/.config/hypr/scripts/gpuinfo.sh
+++ Configs/.config/hypr/scripts/gpuinfo.sh
@@ -1,46 +1,176 @@
 #!/bin/bash
+# shellcheck disable=SC2312 
+# shellcheck disable=SC1090
+gpuQ="/tmp/hyprdots-${UID}-gpuinfo-query"
+tired=false
+[[ " $* " =~ " tired " ]] && ! grep -q "tired" "${gpuQ}" && echo "tired=true" >>"${gpuQ}"
+if [[ ! " $* " =~ " startup " ]]; then
+   gpuQ="${gpuQ}$2"
+fi
+detect() { # Auto detect Gpu used by Hyprland(declared using env = WLR_DRM_DEVICES) Sophisticated? 
+card=$(echo "${WLR_DRM_DEVICES}" | cut -d':' -f1 | cut -d'/' -f4)
+# shellcheck disable=SC2010
+slot_number=$(ls -l /dev/dri/by-path/ | grep "${card}" | awk -F'pci-0000:|-card' '{print $2}')
+vendor_id=$(lspci -nn -s "${slot_number}" )
+declare -A vendors=(["10de"]="nvidia" ["8086"]="intel" ["1002"]="amd")
+for vendor in "${!vendors[@]}"; do
+   if [[ ${vendor_id} == *"${vendor}"* ]]; then
+       initGPU="${vendors[${vendor}]}"
+       break
+   fi
+done
+if [[ -n ${initGPU} ]]; then
+ $0 --use "${initGPU}" startup
+fi
+}
 
-# Check for NVIDIA GPU using nvidia-smi
+query() { 
+ nvidia_flag=0 amd_flag=0 intel_flag=0
+touch "${gpuQ}" 
+
+if lsmod | grep -q 'nouveau'; then 
+      echo "nvidia_gpu=\"Linux\"" >>"${gpuQ}" #? Incase If nouveau is installed 
+      echo "nvidia_flag=1 # Using nouveau an open-source nvidia driver" >>"${gpuQ}"
+elif  command -v nvidia-smi &> /dev/null; then
 nvidia_gpu=$(nvidia-smi --query-gpu=gpu_name --format=csv,noheader,nounits | head -n 1)
+  if [[ -n "${nvidia_gpu}" ]] ; then  # Check for NVIDIA GPU
+      if  [[ "${nvidia_gpu}" == *"NVIDIA-SMI has failed"* ]]; then  #? Second Layer for dGPU 
+        echo "nvidia_flag=0 # NVIDIA-SMI has failed" >> "${gpuQ}"
+      else
+        nvidia_address=$(lspci | grep -Ei "VGA|3D" | grep -i "${nvidia_gpu/NVIDIA /}" | cut -d' ' -f1)
+        {   echo "nvidia_address=\"${nvidia_address}\""
+            echo "nvidia_gpu=\"${nvidia_gpu/NVIDIA /}\""
+            echo "nvidia_flag=1"
+        } >> "${gpuQ}"
+      fi
+  fi
+fi
 
-# Function to execute the AMD GPU Python script and use its output
-execute_amd_script() {
-  local amd_output
-  amd_output=$(python3 ~/.config/hypr/scripts/amdgpu.py)
-  echo "$amd_output"
+if lspci -nn | grep -E "(VGA|3D)" | grep -iq "1002"; then 
+amd_gpu="$(lspci -nn | grep -Ei "VGA|3D" | grep -m 1 "1002" | awk -F'Advanced Micro Devices, Inc. ' '{gsub(/ *\[[^\]]*\]/,""); gsub(/ *\([^)]*\)/,""); print $2}')"
+amd_address=$(lspci | grep -Ei "VGA|3D" | grep -i "${amd_gpu}" | cut -d' ' -f1)
+{ echo "amd_address=\"${amd_address}\"" 
+  echo "amd_flag=1" # Check for Amd GPU 
+  echo "amd_gpu=\"${amd_gpu}\""
+} >> "${gpuQ}";fi
+
+if lspci -nn | grep -E "(VGA|3D)" | grep -iq "8086"; then 
+intel_gpu="$(lspci -nn | grep -Ei "VGA|3D" | grep -m 1 "8086" | awk -F'Intel Corporation ' '{gsub(/ *\[[^\]]*\]/,""); gsub(/ *\([^)]*\)/,""); print $2}')"
+intel_address=$(lspci | grep -Ei "VGA|3D" | grep -i "${intel_gpu}" | cut -d' ' -f1)
+{ echo "intel_address=\"${intel_address}\"" 
+  echo "intel_flag=1"  # Check for Intel GPU
+  echo "intel_gpu=\"${intel_gpu}\"" 
+} >>"${gpuQ}"; fi
+
+if ! grep -q "prioGPU=" "${gpuQ}" && [[ -n "${WLR_DRM_DEVICES}" ]]; then 
+  trap detect EXIT
+fi
+
 }
 
-# Function to get Intel GPU temperature from 'sensors'
-get_intel_gpu_temperature() {
-  local temperature
-  temperature=$(sensors | grep "Package id 0" | awk '{print $4}' | sed 's/+//;s/°C//;s/\.0//')
-  echo "$temperature"
-}
-
-# Function to define emoji based on temperature
-get_temperature_emoji() {
-  local temperature="$1"
-  if [ "$temperature" -lt 60 ]; then
-    echo "❄️"  # Ice emoji for less than 60°C
+toggle() {
+  if [[ -n "$1" ]]; then
+      next_prioGPU="$1_flag"
   else
-    echo "🔥"  # Fire emoji for 60°C or higher
+    # Initialize gpu_flags and prioGPU if they don't exist
+    if ! grep -q "gpu_flags=" "${gpuQ}"; then
+        gpu_flags=$(grep "flag=1" "${gpuQ}" | cut -d '=' -f 1 | tr '\n' ' ' | tr -d '#')
+        echo "" >> "${gpuQ}"
+        echo "gpu_flags=\"${gpu_flags[*]}\"" >> "${gpuQ}"
+    fi
+
+    if ! grep -q "prioGPU=" "${gpuQ}"; then
+        gpu_flags=$(grep "gpu_flags=" "${gpuQ}"  | cut -d'=' -f 2)
+        initGPU=$(echo "${gpu_flags}" | cut -d ' ' -f  1)
+        echo "prioGPU=${initGPU}" >> "${gpuQ}"
+    fi
+    mapfile -t anchor < <(grep "flag=1" "${gpuQ}" | cut -d '=' -f 1)
+    prioGPU=$(grep "prioGPU=" "${gpuQ}" | cut -d'=' -f 2)    # Get the current prioGPU from the file
+    # Find the index of the current prioGPU in the anchor array
+    for index in "${!anchor[@]}"; do
+        if [[ "${anchor[${index}]}" = "${prioGPU}" ]]; then
+            current_index=${index}
+        fi
+    done
+    next_index=$(( (current_index + 1) % ${#anchor[@]} ))
+    next_prioGPU=${anchor[${next_index}]#\#}
+fi
+
+# Set the next prioGPU and remove the '#' character
+sed -i 's/^\(nvidia_flag=1\|amd_flag=1\|intel_flag=1\)/#\1/' "${gpuQ}" # Comment out all the gpu flags in the file
+sed -i "s/^#${next_prioGPU}/${next_prioGPU}/" "${gpuQ}" # Uncomment the next prioGPU in the file
+sed -i "s/prioGPU=${prioGPU}/prioGPU=${next_prioGPU}/" "${gpuQ}" # Update the prioGPU in the file
+}
+
+get_temperature_emoji() { # Function to define emoji based on temperature
+  local temperature="$1"
+  if [[ "${temperature}" -lt 60 ]]; then
+    echo ""  # Ice emoji for less than 60°C
+  else
+    echo "󰈸"  # Fire emoji for 60°C or higher
   fi
 }
 
-# Check if primary GPU is NVIDIA
-if [ -n "$nvidia_gpu" ]; then
-  # if nvidia-smi failed, format and exit. 
-  if [[ $nvidia_gpu == *"NVIDIA-SMI has failed"* ]]; then
-    # Print the formatted information in JSON
-    echo "{\"text\":\"N/A\", \"tooltip\":\"Primary GPU: Not found\"}"
-    exit 0
+generate_json() {
+  emoji=$(get_temperature_emoji "${temperature}")
+  local json="{\"text\":\"${temperature}°C\", \"tooltip\":\"GPU: ${primary_gpu}\n${emoji} Temperature: ${temperature}°C"
+#? Soon Add Something incase needed.
+  declare -A tooltip_parts
+  if [[ -n "${utilization}" ]]; then tooltip_parts["\n󰾆 Utilization: "]="${utilization}%" ; fi
+  if [[ -n "${current_clock_speed}" ]] && [[ -n "${max_clock_speed}" ]]; then tooltip_parts["\n Clock Speed: "]="${current_clock_speed}/${max_clock_speed} MHz" ; fi
+  if [[ -n "${gpu_load}" ]]; then tooltip_parts["\n󰾆 Utilization: "]="${gpu_load}%" ; fi
+  if [[ -n "${core_clock}" ]]; then tooltip_parts["\n Clock Speed: "]="${core_clock} MHz" ;fi
+  if [[ -n "${power_usage}" ]]; then if [[ -n "${power_limit}" ]]; then
+                                    tooltip_parts["\n Power Usage: "]="${power_usage}/${power_limit} W"
+                                  else
+                                    tooltip_parts["\n Power Usage: "]="${power_usage} W"
+                                  fi
   fi
+  if [[ -n "${power_discharge}" ]] && [[ "${power_discharge}" != "0" ]]; then tooltip_parts["\n Power Discharge: "]="${power_discharge} W" ;fi
 
-  primary_gpu="NVIDIA GPU"
-  # Collect GPU information for NVIDIA
+  for key in "${!tooltip_parts[@]}"; do
+    local value="${tooltip_parts[${key}]}"
+    if [[ -n "${value}" && "${value}" =~ [a-zA-Z0-9] ]]; then
+      json+="${key}${value}"
+    fi
+  done
+  json="${json}\"}"
+  echo "${json}"
+}
+
+general_query() { # Function to get temperature from 'sensors'
+	filter=''	
+temperature=$(sensors | ${filter} grep -m 1 -E "(edge|Package id.*|another keyword)" | awk -F ':' '{print int($2)}') #! We can get json data from sensors too
+  # gpu_load=$()
+  # core_clock=$()
+for file in /sys/class/power_supply/BAT*/power_now; do
+    [[ -f "${file}" ]] && power_discharge=$(awk '{print $1*10^-6 ""}' "${file}") && break
+done
+[[ -z "${power_discharge}" ]] && for file in /sys/class/power_supply/BAT*/current_now; do
+    [[ -f "${file}" ]] && power_discharge=$(awk -v current="$(cat "${file}")" -v voltage="$(cat "${file/current_now/voltage_now}")" 'BEGIN {print (current * voltage) / 10^12 ""}') && break
+done
+# power_limit=$()
+utilization=$(top -bn1 | grep "Cpu(s)" | sed "s/.*, *\([0-9.]*\)%* id.*/\1/" | awk '{print 100 - $1" "}')
+current_clock_speed=$(awk '{sum += $1; n++} END {if (n > 0) print sum / n / 1000 ""}' /sys/devices/system/cpu/cpufreq/policy*/scaling_cur_freq)
+max_clock_speed=$(awk '{print $1/1000}' /sys/devices/system/cpu/cpu0/cpufreq/cpuinfo_max_freq)
+}
+
+intel_GPU() { #? Function to query basic intel GPU
+    primary_gpu="INTEL ${intel_gpu}"
+    general_query
+}
+
+nvidia_GPU() { #? Function to query Nvidia GPU
+    primary_gpu="NVIDIA ${nvidia_gpu}"
+  if [[ "${nvidia_gpu}" == "Linux" ]]; then general_query ; return ; fi #? Open source driver
+#? Tired Flag for not using nvidia-smi if GPU is in suspend mode. 
+if ${tired}; then is_suspend="$(cat /sys/bus/pci/devices/0000:"${nvidia_address}"/power/runtime_status)"
+   if [[ ${is_suspend} == *"suspend"* ]]; then
+      printf '{"text":"󰤂", "tooltip":"%s ⏾ Suspended mode"}' "${primary_gpu}"; exit ;fi
+fi
   gpu_info=$(nvidia-smi --query-gpu=temperature.gpu,utilization.gpu,clocks.current.graphics,clocks.max.graphics,power.draw,power.max_limit --format=csv,noheader,nounits)
   # Split the comma-separated values into an array
-  IFS=',' read -ra gpu_data <<< "$gpu_info"
+  IFS=',' read -ra gpu_data <<< "${gpu_info}"
   # Extract individual values
   temperature="${gpu_data[0]// /}"
   utilization="${gpu_data[1]// /}"
@@ -48,42 +178,75 @@ if [ -n "$nvidia_gpu" ]; then
   max_clock_speed="${gpu_data[3]// /}"
   power_usage="${gpu_data[4]// /}"
   power_limit="${gpu_data[5]// /}"
+}
 
-  # Get emoji based on temperature
-  emoji=$(get_temperature_emoji "$temperature")
-
-  # Print the formatted information in JSON
-  echo "{\"text\":\"$temperature°C\", \"tooltip\":\"Primary GPU: $primary_gpu\n$emoji Temperature: $temperature°C\n󰾆 Utilization: $utilization%\n Clock Speed: $current_clock_speed/$max_clock_speed MHz\n Power Usage: $power_usage/$power_limit W\"}"
-else
-  # Execute the AMD GPU Python script and use its output
-  amd_output=$(execute_amd_script)
+amd_GPU() { #? Funtion to query amd GPU
+  primary_gpu="AMD ${amd_gpu}"
+    # Execute the AMD GPU Python script and use its output
+  amd_output=$(python3 ~/.config/hypr/scripts/amdgpu.py)
+if [[ ! ${amd_output} == *"No AMD GPUs detected."* ]] && [[ ! ${amd_output} == *"Unknown query failure"* ]]; then #! This will be changed if "(python3 ~/.config/hypr/scripts/amdgpu.py)" Changes!
   # Extract GPU Temperature, GPU Load, GPU Core Clock, and GPU Power Usage from amd_output
-  temperature=$(echo "$amd_output" | jq -r '.["GPU Temperature"]' | sed 's/°C//')
-  gpu_load=$(echo "$amd_output" | jq -r '.["GPU Load"]' | sed 's/%//')
-  core_clock=$(echo "$amd_output" | jq -r '.["GPU Core Clock"]' | sed 's/ GHz//;s/ MHz//')
-  power_usage=$(echo "$amd_output" | jq -r '.["GPU Power Usage"]' | sed 's/ Watts//')
+  temperature=$(echo "${amd_output}" | jq -r '.["GPU Temperature"]' | sed 's/°C//')
+  gpu_load=$(echo "${amd_output}" | jq -r '.["GPU Load"]' | sed 's/%//')
+  core_clock=$(echo "${amd_output}" | jq -r '.["GPU Core Clock"]' | sed 's/ GHz//;s/ MHz//')
+  power_usage=$(echo "${amd_output}" | jq -r '.["GPU Power Usage"]' | sed 's/ Watts//')
 
-  # Get emoji based on temperature
-  emoji=$(get_temperature_emoji "$temperature")
+# elif  #? Can add another Layer of query if "~/.config/hypr/scripts/amdgpu.py" fails
 
-  # Print the formatted information in JSON
-  if [ -n "$temperature" ]; then
-    primary_gpu="AMD GPU"
-    echo "{\"text\":\"$temperature°C\", \"tooltip\":\"Primary GPU: $primary_gpu\n$emoji Temperature: $temperature°C\n󰾆 Utilization: $gpu_load%\n Clock Speed: $core_clock MHz\n Power Usage: $power_usage W\"}"
-  else
-    # Check for Intel GPU
-    primary_gpu="Intel GPU"
-    intel_gpu=$(lspci -nn | grep -i "VGA compatible controller" | grep -i "Intel Corporation" | awk -F' ' '{print $1}')
-    if [ -n "$intel_gpu" ]; then
-      temperature=$(get_intel_gpu_temperature)
-      emoji=$(get_temperature_emoji "$temperature")
-      # Print the formatted information in JSON
-      echo "{\"text\":\"$temperature°C\", \"tooltip\":\"Primary GPU: $primary_gpu\n$emoji Temperature: $temperature°C\"}"
-    else
-      primary_gpu="Not found"
-      gpu_info=""
-      # Print the formatted information in JSON
-      echo "{\"text\":\"N/A\", \"tooltip\":\"Primary GPU: $primary_gpu\"}"
-    fi
-  fi
+else
+general_query
 fi
+}
+
+if [[ ! -f "${gpuQ}" ]]; then  
+query ; echo -e "Initialized Variable:\n$(cat "${gpuQ}")\n\nReboot or '$0 --reset' to RESET Variables"
+fi
+source "${gpuQ}"
+case "$1" in
+  "--toggle"|"-t")
+      toggle
+echo -e "Sensor: ${next_prioGPU} GPU" | sed 's/_flag//g' 
+ exit
+    ;;
+  "--use"|"-u")
+      toggle "$2"
+    ;;
+  "--reset"|"-rf")
+    rm -fr "${gpuQ}"*
+    query
+    echo -e "Initialized Variable:\n$(cat "${gpuQ}" || true)\n\nReboot or '$0 --reset' to RESET Variables"
+    exit
+    ;;
+  *"-"*)
+gpu_flags=$(grep "flag=1" "${gpuQ}" | cut -d '=' -f 1 | tr '\n' ' ' | tr -d '#')
+cat << EOF
+
+Avalable GPU: ${gpu_flags//_flag/}
+[options]
+--toggle         * Toggle available GPU
+--use [GPU]      * Only call the specified GPU (Useful for adding specific GPU on waybar)
+--reset          *  Remove & restart all query
+
+[flags]
+tired            * Adding this option will not query nvidia-smi if gpu is in suspend mode 
+startup          * Useful if you want a certain GPU to be set at startup
+
+* If ${USER} declared env = WLR_DRM_DEVICES on hyprland then use this as the primary GPU
+EOF
+exit
+    ;;
+esac
+
+nvidia_flag=${nvidia_flag:-0} intel_flag=${intel_flag:-0} amd_flag=${amd_flag:-0}
+#? Based on the flags, call the corresponding function multi flags means multi GPU.
+if [[ "${nvidia_flag}" -eq 1 ]]; then 
+  nvidia_GPU
+elif [[ "${amd_flag}" -eq 1 ]]; then 
+  amd_GPU
+elif [[ "${intel_flag}" -eq 1 ]]; then 
+  intel_GPU
+else primary_gpu="Not found"
+  general_query
+fi
+
+generate_json #? AutoGen the Json txt for Waybar
