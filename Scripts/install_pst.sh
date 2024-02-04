5392f25ddcc4c4fdfe103c8af3229885912382c9
tittu <prasanthrangan@rediffmail.com>
2024-02-04 20:16:53 +0530

    adding sddm theme option

--- Scripts/install_pst.sh
+++ Scripts/install_pst.sh
@@ -15,27 +15,36 @@ fi
 if pkg_installed sddm
     then
 
+    echo -e "\033[0;32m[DISPLAYMANAGER]\033[0m detected // sddm"
     if [ ! -d /etc/sddm.conf.d ] ; then
         sudo mkdir -p /etc/sddm.conf.d
     fi
 
     if [ ! -f /etc/sddm.conf.d/kde_settings.t2.bkp ] ; then
-        echo "configuring sddm..."
-        sudo tar -xzf ${CloneDir}/Source/arcs/Sddm_Corners.tar.gz -C /usr/share/sddm/themes/
+        echo -e "\033[0;32m[DISPLAYMANAGER]\033[0m configuring sddm..."
+        echo -e "Select sddm theme:\n1) Candy\n2) Corners"
+        read -p "Enter option number : " sddmopt
+
+        case $sddmopt in
+        1) sddmtheme="Candy";;
+        *) sddmtheme="Corners";;
+        esac
+
+        sudo tar -xzf ${CloneDir}/Source/arcs/Sddm_${sddmtheme}.tar.gz -C /usr/share/sddm/themes/
         sudo touch /etc/sddm.conf.d/kde_settings.conf
         sudo cp /etc/sddm.conf.d/kde_settings.conf /etc/sddm.conf.d/kde_settings.t2.bkp
-        sudo cp /usr/share/sddm/themes/corners/kde_settings.conf /etc/sddm.conf.d/
+        sudo cp /usr/share/sddm/themes/${sddmtheme}/kde_settings.conf /etc/sddm.conf.d/
     else
-        echo "sddm is already configured..."
+        echo -e "\033[0;33m[SKIP]\033[0m sddm is already configured..."
     fi
 
     if [ ! -f /usr/share/sddm/faces/${USER}.face.icon ] && [ -f ${CloneDir}/Source/misc/${USER}.face.icon ] ; then
         sudo cp ${CloneDir}/Source/misc/${USER}.face.icon /usr/share/sddm/faces/
-        echo "avatar set for ${USER}..."
+        echo -e "\033[0;32m[DISPLAYMANAGER]\033[0m avatar set for ${USER}..."
     fi
 
 else
-    echo "WARNING: sddm is not installed..."
+    echo -e "\033[0;33m[WARNING]\033[0m sddm is not installed..."
 fi
 
 
@@ -43,11 +52,12 @@ fi
 if pkg_installed dolphin && pkg_installed xdg-utils
     then
 
+    echo -e "\033[0;32m[FILEMANAGER]\033[0m detected // dolphin"
     xdg-mime default org.kde.dolphin.desktop inode/directory
-    echo "setting" `xdg-mime query default "inode/directory"` "as default file explorer..."
+    echo -e "\033[0;32m[FILEMANAGER]\033[0m setting" `xdg-mime query default "inode/directory"` "as default file explorer..."
 
 else
-    echo "WARNING: dolphin is not installed..."
+    echo -e "\033[0;33m[WARNING]\033[0m dolphin is not installed..."
 fi
 
 
