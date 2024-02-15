9b9b1ce8cdd5914f243f62f19d38c6c2d457fd8b
tittu <prasanthrangan@rediffmail.com>
2024-02-15 17:56:30 +0530

    refactor cpuinfo script

--- Configs/.config/hypr/scripts/cpuinfo.sh
+++ Configs/.config/hypr/scripts/cpuinfo.sh
@@ -1,68 +1,29 @@
-#!/bin/bash
+#!/usr/bin/env sh
 
-# Thee shalt find the greatest one,
-# He who not more than the chosen one
-map_floor() {
+# CPU model
+model=$(lscpu | awk -F ':' '/Model name/ {sub(/^ *| *$/,"",$2); print $2}' | awk '{NF-=3}1')
 
-    # From the depths of the string, words arise,
-    # Keys in pairs, a treasure in disguise.
-    IFS=', ' read -r -a pairs <<< "$1"
-
-    # If the final token stands alone and bold,
-    # Declare it the default, its worth untold.
-    if [[ ${pairs[-1]} != *":"* ]]; then
-        def_val="${pairs[-1]}"
-        unset 'pairs[${#pairs[@]}-1]'
-    fi
-
-    # Scans the map, a peak it seeks,
-    # The highest passed, the value speaks.
-    for pair in "${pairs[@]}"; do
-        IFS=':' read -r key value <<< "$pair"
-        
-        # Behold! Thou holds the secrets they seek,
-        # Declare it and silence the whispers unique.
-        if awk -v num="$2" -v k="$key" 'BEGIN { exit !(num > k) }'; then
-            echo "$value"
-            return
-        fi
-    done
-
-    # On this lonely shore, where silence dwells
-    # Even the waves, echoes words unheard
-    [ -n "$def_val" ] && echo $def_val || echo " "
-}
-
-# Generate emoji and icon based on temperature and utilization
-get_icons() {
-    # key-value pairs of temperature and utilization levels
-    temp_lv="85:&🌋, 65:&🔥, 45:&☁️, &❄️"
-    util_lv="90:, 60:󰓅, 30:󰾅, 󰾆" 
-
-    # return comma seperated emojis/icons 
-    icons=$(map_floor "$temp_lv" $1 | sed "s/&/,/")
-    icons="$icons,$(map_floor "$util_lv" $2)"
-    echo $icons
-}
-
-# Get CPU frequency
-get_freq() {
-  freqlist=$(cat /proc/cpuinfo | grep "cpu MHz" | awk '{ print $4 }')
-  maxfreq=$(lscpu | grep "CPU max MHz" | awk -F: '{ print $2}' | sed -e 's/ //g' -e 's/\.[0-9]*//g')
-  echo $(echo $freqlist | tr ' ' '\n' | awk "{ sum+=\$1 } END {printf \"%.0f/$maxfreq MHz\", sum/NR}")
-}
-
-# Get CPU information
-model=$(lscpu | awk -F': ' '/Model name/ {sub(/^ *| *$/,"",$2); print $2}' | awk '{NF-=3}1')
+# CPU utilization
 utilization=$(top -bn1 | awk '/^%Cpu/ {print 100 - $8}')
-frequency=$(get_freq)
-temp=$(sensors | grep "Package id 0" | awk '{print $4}' | sed 's/+//;s/°C//;s/\.0//')
 
-# Get emoji and icon based on temperature and utilization
-icons=$(get_icons "$temp" "$utilization")
-thermo=$(echo $icons | awk -F, '{print $1}')
-emoji=$(echo $icons | awk -F, '{print $2}')
-speedo=$(echo $icons | awk -F, '{print $3}')
+# Clock speed
+freqlist=$(cat /proc/cpuinfo | grep "cpu MHz" | awk '{ print $4 }')
+maxfreq=$(lscpu | grep "CPU max MHz" | awk -F: '{ print $2}' | sed -e 's/ //g' -e 's/\.[0-9]*//g')
+frequency=$(echo $freqlist | tr ' ' '\n' | awk "{ sum+=\$1 } END {printf \"%.0f/$maxfreq MHz\", sum/NR}")
 
-# Print the formatted information in JSON
-echo "{\"text\":\"$thermo $temp°C\", \"tooltip\":\"$model\n$thermo Temperature: $temp°C $emoji\n$speedo Utilization: $utilization%\n Clock Speed: $frequency\"}"
+# CPU temp
+temp=$(sensors | grep "Package id 0" | awk -F '[+.]' '{print $2}')
+
+# map icons
+set_ico="{\"thermo\":{\"0\":\"\",\"45\":\"\",\"65\":\"\",\"85\":\"\"},\"emoji\":{\"0\":\"❄\",\"45\":\"☁\",\"65\":\"\",\"85\":\"\"},\"util\":{\"0\":\"󰾆\",\"30\":\"󰾅\",\"60\":\"󰓅\",\"90\":\"\"}}"
+eval_ico() {
+    map_ico=$(echo "${set_ico}" | jq -r --arg aky "$1" --argjson avl "$2" '.[$aky] | keys_unsorted | map(tonumber) | map(select(. <= $avl)) | max')
+    echo "${set_ico}" | jq -r --arg aky "$1" --arg avl "$map_ico" '.[$aky] | .[$avl]'
+}
+
+thermo=$(eval_ico thermo $temp)
+emoji=$(eval_ico emoji $temp)
+speedo=$(eval_ico util $utilization)
+
+# Print cpu info (json)
+echo "{\"text\":\"${thermo} ${temp}°C\", \"tooltip\":\"${model}\n${thermo} Temperature: ${temp}°C ${emoji}\n${speedo} Utilization: ${utilization}%\n Clock Speed: ${frequency}\"}"
