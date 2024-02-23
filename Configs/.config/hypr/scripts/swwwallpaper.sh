1487da80a8ee741177b1f7ab1520bd644edbbb57
tittu <prasanthrangan@rediffmail.com>
2024-02-23 21:02:58 +0530

    minor updates

--- Configs/.config/hypr/scripts/swwwallpaper.sh
+++ Configs/.config/hypr/scripts/swwwallpaper.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env sh
 
+# lock instance
+
 lockFile="/tmp/hyrpdots$(id -u)swwwallpaper.lock"
- [ -e "$lockFile" ] &&  notify-send -a "Hyprdots" "An instance of the script is already running..." && exit  1 
+[ -e "$lockFile" ] && echo "An instance of the script is already running..." && exit 1
 touch "${lockFile}"
-trap 'rm -f ${lockFile}'  EXIT
+trap 'rm -f ${lockFile}' EXIT
+
 
 # define functions
 
