7c9cb41054d747860303ae820fbf3dba9e6f3146
tittu <prasanthrangan@rediffmail.com>
2024-03-04 02:48:34 +0530

    wallbash v3 color refresh

--- Configs/.config/hypr/scripts/swwwallbash.sh
+++ Configs/.config/hypr/scripts/swwwallbash.sh
@@ -4,94 +4,42 @@
 
 export ScrDir=`dirname "$(realpath "$0")"`
 source $ScrDir/globalcontrol.sh
-dcoDir="${XDG_CONFIG_HOME:-$HOME/.config}/hypr/wallbash"
-input_wall="$1"
-export cacheImg=$(basename "${input_wall}")
+wallbashDir="${XDG_CONFIG_HOME:-$HOME/.config}/hypr/wallbash/"
+wallbashImg="$1"
+cacheImg=$(basename "${wallbashImg}")
+cacheThm=$(dirname "${wallbashImg}" | awk -F '/' '{print $NF}')
+export wallbashOut="${cacheDir}/${cacheThm}/${cacheImg}.dcol"
 
-if [ -z "${input_wall}" ] || [ ! -f "${input_wall}" ] ; then
+
+# validate input
+
+if [ -z "${wallbashImg}" ] || [ ! -f "${wallbashImg}" ] ; then
     echo "Error: Input wallpaper not found!"
     exit 1
 fi
 
-
-# color conversion functions
-
-hex_conv() {
-    rgb_val=$(echo "$1" | sed 's/[-srgb()%]//g ; s/,/ /g')
-    red=$(echo "$rgb_val * 255 / 100" | awk '{printf "%d", $1}')
-    green=$(echo "$rgb_val * 255 / 100" | awk '{printf "%d", $2}')
-    blue=$(echo "$rgb_val * 255 / 100" | awk '{printf "%d", $3}')
-    printf "%02X%02X%02X\n" "$red" "$green" "$blue"
-}
-
-rgb_negative() {
-    local rgb_val=$1
-    red=${rgb_val:0:2}
-    green=${rgb_val:2:2}
-    blue=${rgb_val:4:2}
-    red_dec=$((16#$red))
-    green_dec=$((16#$green))
-    blue_dec=$((16#$blue))
-    negative_red=$(printf "%02X" $((255 - $red_dec)))
-    negative_green=$(printf "%02X" $((255 - $green_dec)))
-    negative_blue=$(printf "%02X" $((255 - $blue_dec)))
-    echo "${negative_red}${negative_green}${negative_blue}"
-}
-
-dark_light() {
-    inCol="$1"
-    red=$(printf "%d" "0x${inCol:1:2}")
-    green=$(printf "%d" "0x${inCol:3:2}")
-    blue=$(printf "%d" "0x${inCol:5:2}")
-    brightness=$((red + green + blue))
-    [ "$brightness" -lt 250 ]
-}
-
-
-# extract 3 dominant colors and generate accent colors
-
-if [ ! -f "${cacheDir}/${gtkTheme}/${cacheImg}.dcol" ] ; then
-    mkdir -p "${cacheDir}/${gtkTheme}"
-    readarray -t dcol <<< $(magick "${input_wall}"[0] -colors 3 -define histogram:unique-colors=true -format "%c" histogram:info: | sed -e 's/.*\(#[0-9A-F]\+\).*/\1/' | cut -c2-7 | sort)
-    for (( i = 1; i < 3; i++ )) ; do
-        [ -z "${dcol[i]}" ] && dcol[i]=${dcol[i-1]}
-    done
-
-    for (( j = 0; j < 3; j++ )) ; do
-        r_swatch=$(echo "#${dcol[j]}" | sed 's/#//g')
-        echo "dcol_pry${j}=\"${r_swatch}\"" >> "${cacheDir}/${gtkTheme}/${cacheImg}.dcol"
-        r_swatch=$(rgb_negative ${dcol[j]})
-        echo "dcol_txt${j}=\"${r_swatch}\"" >> "${cacheDir}/${gtkTheme}/${cacheImg}.dcol"
-        z=0
-
-        if dark_light "#${dcol[j]}" ; then
-            echo "#${dcol[j]} // pry${j}-[dark]"
-            for t in 25 45 65 85 ; do
-                z=$(( z + 1 ))
-                r_swatch=$(hex_conv `convert xc:"#${dcol[j]}" -modulate 70,"$(awk "BEGIN {print $t * 1.5}")",$(( 100 + (6*z) )) -channel RGB -evaluate multiply 3.$t -format "%c" histogram:info: | awk '{print $4}'`)
-                echo "dcol_${j}xa${z}=\"${r_swatch}\"" >> "${cacheDir}/${gtkTheme}/${cacheImg}.dcol"
-            done
-        else
-            echo "#${dcol[j]} // pry${j}-[light]"
-            for t in 20 40 60 80 ; do
-                z=$(( z + 1 ))
-                r_swatch=$(hex_conv `convert xc:"#${dcol[j]}" -modulate 100,"$(awk "BEGIN {print $t * 1.5}")",$(( 100 + (3*z) )) -channel RGB -evaluate multiply 1.$t -format "%c" histogram:info: | awk '{print $4}'`)
-                echo "dcol_${j}xa${z}=\"${r_swatch}\"" >> "${cacheDir}/${gtkTheme}/${cacheImg}.dcol"
-            done
-        fi
-    done
-    cat "${cacheDir}/${gtkTheme}/${cacheImg}.dcol"
+magick -ping "${wallbashImg}" -format "%t" info: &> /dev/null
+if [ $? -ne 0 ] ; then
+    echo "Error: Unsuppoted image format ${wallbashImg}"
+    exit 1
 fi
 
 
-# wallbash fn to apply colors to templates
+# generate wallbash colors
+
+if [ ! -f "${wallbashOut}" ] ; then
+    $ScrDir/wallbash.sh "${wallbashImg}"
+fi
+
+
+# deploy wallbash colors
 
 fn_wallbash () {
     local tplt="${1}"
     eval target=$(head -1 "${tplt}" | awk -F '|' '{print $1}')
     eval appexe=$(head -1 "${tplt}" | awk -F '|' '{print $2}')
     source "${ScrDir}/globalcontrol.sh"
-    source "${cacheDir}/${gtkTheme}/${cacheImg}.dcol"
+    source "${wallbashOut}"
 
     sed '1d' "${tplt}" > "${target}"
     sed -i "s/<wallbash_pry0>/${dcol_pry0}/g
@@ -111,7 +59,13 @@ fn_wallbash () {
             s/<wallbash_2xa1>/${dcol_2xa1}/g
             s/<wallbash_2xa2>/${dcol_2xa2}/g
             s/<wallbash_2xa3>/${dcol_2xa3}/g
-            s/<wallbash_2xa4>/${dcol_2xa4}/g" "${target}"
+            s/<wallbash_2xa4>/${dcol_2xa4}/g
+            s/<wallbash_pry3>/${dcol_pry2}/g
+            s/<wallbash_txt3>/${dcol_txt2}/g
+            s/<wallbash_3xa1>/${dcol_2xa1}/g
+            s/<wallbash_3xa2>/${dcol_2xa2}/g
+            s/<wallbash_3xa3>/${dcol_2xa3}/g
+            s/<wallbash_3xa4>/${dcol_2xa4}/g" "${target}"
 
     if [ ! -z "${appexe}" ] ; then
         "${appexe}"
@@ -123,5 +77,5 @@ export -f fn_wallbash
 
 # exec wallbash fn in parallel
 
-find "$dcoDir" -type f -name "*.dcol" | parallel -j 0 fn_wallbash
+find "${wallbashDir}" -type f -name "*.dcol" | parallel -j 0 fn_wallbash
 
---
+++ Scripts/wallbash.sh
@@ -0,0 +1,90 @@
+#!/bin/bash
+#|---/ /+---------------------------------------------+---/ /|#
+#|--/ /-| Script to generate color palette from image |--/ /-|#
+#|-/ /--| Prasanth Rangan                             |-/ /--|#
+#|/ /---+---------------------------------------------+/ /---|#
+
+
+# set variables
+
+cacheDir="$HOME/.cache/hyprdots"
+wallbashImg="${1}"
+wallbashColors=4
+wallbashAccent=4
+wallbashFuzz=50
+cacheImg=$(basename "${wallbashImg}")
+cacheThm=$(dirname "${wallbashImg}" | awk -F '/' '{print $NF}')
+wallbashRaw="${cacheDir}/${cacheThm}/${cacheImg}.mpc"
+wallbashOut="${cacheDir}/${cacheThm}/${cacheImg}.dcol"
+
+
+# validate input
+
+if [ -z "${wallbashImg}" ] || [ ! -f "${wallbashImg}" ] ; then
+    echo "Error: Input wallpaper not found!"
+    exit 1
+fi
+
+magick -ping "${wallbashImg}" -format "%t" info: &> /dev/null
+if [ $? -ne 0 ] ; then
+    echo "Error: Unsuppoted image format ${wallbashImg}"
+    exit 1
+fi
+
+echo "wallbash // Colors ${wallbashColors} :: Fuzzy ${wallbashFuzz} :: \"${wallbashOut}\""
+mkdir -p "${cacheDir}/${cacheThm}"
+> "${wallbashOut}"
+
+
+# negative function
+
+rgb_negative() {
+    local rgb_val=$1
+    red=${rgb_val:0:2}
+    green=${rgb_val:2:2}
+    blue=${rgb_val:4:2}
+    red_dec=$((16#$red))
+    green_dec=$((16#$green))
+    blue_dec=$((16#$blue))
+    negative_red=$(printf "%02X" $((255 - $red_dec)))
+    negative_green=$(printf "%02X" $((255 - $green_dec)))
+    negative_blue=$(printf "%02X" $((255 - $blue_dec)))
+    echo "${negative_red}${negative_green}${negative_blue}"
+}
+
+
+# quantize raw primary colors
+
+magick -quiet -regard-warnings "${wallbashImg}"[0] -alpha off +repage "${wallbashRaw}"
+readarray -t dcolRaw <<< $(magick "${wallbashRaw}" -depth 8 -fuzz ${wallbashFuzz}% +dither -colors ${wallbashColors} -depth 8 -format "%c" histogram:info: | sed -n 's/^[ ]*\(.*\):.*[#]\([0-9a-fA-F]*\) .*$/\1,#\2/p' | sort -r -n -k 1 -t "," | awk -F '#' 'length($NF) == 6 {print $NF}')
+dcol=($(echo  -e "${dcolRaw[@]:0:$wallbashColors}" | tr ' ' '\n' | sort))
+[ ${#dcolRaw[*]} -lt ${wallbashColors} ] && echo -e "WARNING :: distinct colors ${#dcolRaw[*]} is less than ${wallbashColors} palette color..."
+
+
+# reformat primary colors cache
+
+for (( i=0; i<${wallbashColors}; i++ )) ; do
+
+    [ -z "${dcol[i]}" ] && dcol[i]=${dcol[i-1]}
+    echo "dcol_pry${i}=\"${dcol[i]}\"" >> "${wallbashOut}"
+    echo "dcol_txt${i}=\"$(rgb_negative ${dcol[i]})\"" >> "${wallbashOut}"
+
+done
+
+
+# loop to generate accent colors
+
+for (( x=1; x<=${wallbashColors}; x++ )) ; do
+    modBri=$(( 100 + (x * 28) ))
+    modSat=$(( 100 - (x * 6 ) ))
+    modHue=$(( 100 - (x * 4 ) ))
+    acntRaw=($(magick "${wallbashRaw}" -depth 8 -modulate ${modBri},${modSat},${modHue} -fuzz ${wallbashFuzz}% +dither -colors ${wallbashAccent} -depth 8 -format "%c" histogram:info: | sed -n 's/^[ ]*\(.*\):.*[#]\([0-9a-fA-F]*\) .*$/\1,#\2/p' | sort -r -n -k 1 -t "," | awk -F '#' 'length($NF) == 6 {print $NF}'))
+    acnt=($(echo "${acntRaw[@]:0:$wallbashAccent}" | tr ' ' '\n' | sort))
+    [ ${#acntRaw[*]} -lt ${wallbashAccent} ] && echo -e "WARNING :: accent colors ${#acntRaw[*]} is less than ${wallbashAccent} palette color..."
+
+    for (( j=0; j<${wallbashAccent}; j++ )) ; do
+        [ -z "${acnt[j]}" ] && acnt[j]=${acnt[j-1]}
+        echo "dcol_${j}xa${x}=\"${acnt[j]}\"" >> "${wallbashOut}"
+    done
+done
+
