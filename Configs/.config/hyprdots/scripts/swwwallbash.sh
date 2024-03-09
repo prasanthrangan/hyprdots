c278e97cedafd9f5d1013e3811f1360938d231e6
tittu <prasanthrangan@rediffmail.com>
2024-03-09 12:26:15 +0530

    wallbash v3 color refresh

--- Configs/.config/hyprdots/scripts/swwwallbash.sh
+++ Configs/.config/hyprdots/scripts/swwwallbash.sh
@@ -47,24 +47,44 @@ fn_wallbash () {
             s/<wallbash_1xa2>/${dcol_1xa2}/g
             s/<wallbash_1xa3>/${dcol_1xa3}/g
             s/<wallbash_1xa4>/${dcol_1xa4}/g
+            s/<wallbash_1xa5>/${dcol_1xa5}/g
+            s/<wallbash_1xa6>/${dcol_1xa6}/g
+            s/<wallbash_1xa7>/${dcol_1xa7}/g
+            s/<wallbash_1xa8>/${dcol_1xa8}/g
+            s/<wallbash_1xa9>/${dcol_1xa9}/g
             s/<wallbash_pry2>/${dcol_pry2}/g
             s/<wallbash_txt2>/${dcol_txt2}/g
             s/<wallbash_2xa1>/${dcol_2xa1}/g
             s/<wallbash_2xa2>/${dcol_2xa2}/g
             s/<wallbash_2xa3>/${dcol_2xa3}/g
             s/<wallbash_2xa4>/${dcol_2xa4}/g
+            s/<wallbash_2xa5>/${dcol_2xa5}/g
+            s/<wallbash_2xa6>/${dcol_2xa6}/g
+            s/<wallbash_2xa7>/${dcol_2xa7}/g
+            s/<wallbash_2xa8>/${dcol_2xa8}/g
+            s/<wallbash_2xa9>/${dcol_2xa9}/g
             s/<wallbash_pry3>/${dcol_pry3}/g
             s/<wallbash_txt3>/${dcol_txt3}/g
             s/<wallbash_3xa1>/${dcol_3xa1}/g
             s/<wallbash_3xa2>/${dcol_3xa2}/g
             s/<wallbash_3xa3>/${dcol_3xa3}/g
             s/<wallbash_3xa4>/${dcol_3xa4}/g
+            s/<wallbash_3xa5>/${dcol_3xa5}/g
+            s/<wallbash_3xa6>/${dcol_3xa6}/g
+            s/<wallbash_3xa7>/${dcol_3xa7}/g
+            s/<wallbash_3xa8>/${dcol_3xa8}/g
+            s/<wallbash_3xa9>/${dcol_3xa9}/g
             s/<wallbash_pry4>/${dcol_pry4}/g
             s/<wallbash_txt4>/${dcol_txt4}/g
             s/<wallbash_4xa1>/${dcol_4xa1}/g
             s/<wallbash_4xa2>/${dcol_4xa2}/g
             s/<wallbash_4xa3>/${dcol_4xa3}/g
-            s/<wallbash_4xa4>/${dcol_4xa4}/g" "${target}"
+            s/<wallbash_4xa4>/${dcol_4xa4}/g
+            s/<wallbash_4xa5>/${dcol_4xa5}/g
+            s/<wallbash_4xa6>/${dcol_4xa6}/g
+            s/<wallbash_4xa7>/${dcol_4xa7}/g
+            s/<wallbash_4xa8>/${dcol_4xa8}/g
+            s/<wallbash_4xa9>/${dcol_4xa9}/g" "${target}"
 
     if [ ! -z "${appexe}" ] ; then
         "${appexe}"
--- Scripts/wallbash.sh
+++ Scripts/wallbash.sh
@@ -10,7 +10,6 @@
 cacheDir="$HOME/.cache/hyprdots"
 wallbashImg="${1}"
 wallbashColors=4
-wallbashAccent=4
 wallbashFuzz=70
 cacheImg=$(basename "${wallbashImg}")
 cacheThm=$(dirname "${wallbashImg}" | awk -F '/' '{print $NF}')
@@ -18,34 +17,17 @@ wallbashRaw="${cacheDir}/${cacheThm}/${cacheImg}.mpc"
 wallbashOut="${cacheDir}/${cacheThm}/${cacheImg}.dcol"
 
 
-#// primary color modulations for default profile
+#// color modulations
 
+defaultCurve="18 99\n32 97\n48 95\n55 90\n70 80\n80 70\n88 60\n94 40\n99 24"
 pryDarkBri=116
 pryDarkSat=110
 pryDarkHue=88
 pryLightBri=100
 pryLightSat=100
 pryLightHue=114
-
-
-#// text color modulations for default profile
-
-txtDarkBri=119
-txtDarkSat=0
-txtDarkHue=100
-txtLightBri=21
-txtLightSat=0
-txtLightHue=100
-
-
-#// accent color modulations for default profile
-
-acnDarkBri=120
-acnDarkSat=111
-acnDarkHue=92
-acnLightBri=100
-acnLightSat=101
-acnLightHue=109
+txtDarkBri=188
+txtLightBri=16
 
 
 #// input image validation
@@ -61,7 +43,7 @@ if [ $? -ne 0 ] ; then
     exit 1
 fi
 
-echo "wallbash default profile :: Colors ${wallbashColors} :: Accents ${wallbashAccent} :: Fuzzy ${wallbashFuzz} :: \"${wallbashOut}\""
+echo "wallbash default profile :: Colors ${wallbashColors} :: Fuzzy ${wallbashFuzz} :: \"${wallbashOut}\""
 mkdir -p "${cacheDir}/${cacheThm}"
 rm -f "${wallbashRaw}" "${wallbashOut}"
 
@@ -96,19 +78,23 @@ fx_brightness() {
 #// quantize raw primary colors
 
 magick -quiet -regard-warnings "${wallbashImg}"[0] -alpha off +repage "${wallbashRaw}"
-readarray -t dcolRaw <<< $(magick "${wallbashRaw}" -depth 8 -fuzz ${wallbashFuzz}% +dither -colors ${wallbashColors} -depth 8 -format "%c" histogram:info: | sed -n 's/^[ ]*\(.*\):.*[#]\([0-9a-fA-F]*\) .*$/\1,#\2/p' | sort -r -n -k 1 -t "," | awk -F '#' 'length($NF) == 6 {print $NF}')
-[ ${#dcolRaw[*]} -lt ${wallbashColors} ] && echo -e "WARNING :: distinct colors ${#dcolRaw[*]} is less than ${wallbashColors} palette color..."
+readarray -t dcolRaw <<< $(magick "${wallbashRaw}" -depth 8 -fuzz ${wallbashFuzz}% +dither -kmeans ${wallbashColors} -depth 8 -format "%c" histogram:info: | sed -n 's/^[ ]*\(.*\):.*[#]\([0-9a-fA-F]*\) .*$/\1,#\2/p' | sort -r -n -k 1 -t "," | awk -F '#' 'length($NF) == 6 {print $NF}')
+
+if [ ${#dcolRaw[*]} -lt ${wallbashColors} ] ; then
+    echo -e "RETRYING :: distinct colors ${#dcolRaw[*]} is less than ${wallbashColors} palette color..."
+    readarray -t dcolRaw <<< $(magick "${wallbashRaw}" -depth 8 -fuzz ${wallbashFuzz}% +dither -colors $((wallbashColors + 2)) -depth 8 -format "%c" histogram:info: | sed -n 's/^[ ]*\(.*\):.*[#]\([0-9a-fA-F]*\) .*$/\1,#\2/p' | sort -r -n -k 1 -t "," | awk -F '#' 'length($NF) == 6 {print $NF}')
+fi
 
 
 #// sort colors based on image brightness
 
 if fx_brightness "${wallbashRaw}" ; then
-    argSort=""
+    colSort=""
 else
-    argSort="-r"
+    colSort="-r"
 fi
 
-dcol=($(echo  -e "${dcolRaw[@]:0:$wallbashColors}" | tr ' ' '\n' | sort ${argSort}))
+dcol=($(echo  -e "${dcolRaw[@]:0:$wallbashColors}" | tr ' ' '\n' | sort ${colSort}))
 
 
 #// loop for derived colors
@@ -140,42 +126,28 @@ for (( i=0; i<${wallbashColors}; i++ )) ; do
 
     #// generate primary text colors
 
-    xcol=$(rgb_negative ${dcol[i]})
+    nTxt=$(rgb_negative ${dcol[i]})
 
     if fx_brightness "xc:#${dcol[i]}" ; then
         modBri=$txtDarkBri
-        modSat=$txtDarkSat
-        modHue=$txtDarkHue
     else
         modBri=$txtLightBri
-        modSat=$txtLightSat
-        modHue=$txtLightHue
     fi
 
-    tcol=$(magick xc:"#${xcol}" -depth 8 -normalize -modulate ${modBri},${modSat},${modHue} -depth 8 -format "%c" histogram:info: | sed -n 's/^[ ]*\(.*\):.*[#]\([0-9a-fA-F]*\) .*$/\1,#\2/p' | sort -r -n -k 1 -t "," | awk -F '#' 'length($NF) == 6 {print $NF}')
+    tcol=$(magick xc:"#${nTxt}" -depth 8 -normalize -modulate ${modBri},10,100 -depth 8 -format "%c" histogram:info: | sed -n 's/^[ ]*\(.*\):.*[#]\([0-9a-fA-F]*\) .*$/\1,#\2/p' | sort -r -n -k 1 -t "," | awk -F '#' 'length($NF) == 6 {print $NF}')
     echo "dcol_txt$((i + 1))=\"${tcol}\"" >> "${wallbashOut}"
 
 
     #// generate accent colors
 
-    xcol="#${dcol[i]}"
+    xHue=$(magick xc:"#${dcol[i]}" -colorspace HSB -format "%c" histogram:info: | awk -F '[hsb(,]' '{print $2}')
+    acnt=1
 
-    for (( j=1; j<=${wallbashAccent}; j++ )) ; do
-
-        if fx_brightness "xc:#${dcol[i]}" ; then
-            modBri=$acnDarkBri
-            modSat=$acnDarkSat
-            modHue=$acnDarkHue
-        else
-            modBri=$acnLightBri
-            modSat=$acnLightSat
-            modHue=$acnLightHue
-        fi
-
-        acnt=$(magick xc:"${xcol}" -depth 8 -normalize -modulate ${modBri},${modSat},${modHue} -depth 8 -format "%c" histogram:info: | sed -n 's/^[ ]*\(.*\):.*[#]\([0-9a-fA-F]*\) .*$/\1,#\2/p' | sort -r -n -k 1 -t "," | awk -F '#' 'length($NF) == 6 {print $NF}')
-        xcol="#${acnt}"
-        echo "dcol_$((i + 1))xa${j}=\"${acnt}\"" >> "${wallbashOut}"
-
+    echo -e "${defaultCurve}" | sort -n ${colSort} | while read -r xBri xSat
+    do
+        acol=$(magick xc:"hsb(${xHue},${xSat}%,${xBri}%)" -depth 8 -format "%c" histogram:info: | sed -n 's/^[ ]*\(.*\):.*[#]\([0-9a-fA-F]*\) .*$/\1,#\2/p' | sort -r -n -k 1 -t "," | awk -F '#' 'length($NF) == 6 {print $NF}')
+        echo "dcol_$((i + 1))xa${acnt}=\"${acol}\"" >> "${wallbashOut}"
+        ((acnt++))
     done
 
 done
