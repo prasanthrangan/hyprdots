2e094cccbe40086b7a2a47dfff8ffcc4d794de90
Jonas Peitz <jonaspeitz@proton.me>
2024-03-19 19:49:25 +0100

    added neovim colorshemes and deps

---
+++ Configs/.config/nvim/lua/plugins/themes.lua
@@ -0,0 +1,63 @@
+return {
+  
+  {
+  "folke/tokyonight.nvim",
+  lazy = false,
+  priority = 1000,
+  opts = {},
+},
+  
+  { "catppuccin/nvim", 
+    name = "catppuccin", 
+    priority = 1000 },
+  -- https://github.com/catppuccin/nvim?tab=readme-ov-file#compile for faster startup
+  { "ellisonleao/gruvbox.nvim", 
+    priority = 1000 , 
+    config = true, 
+    opts = {},  
+  }, 
+   {  "nyoom-engineering/oxocarbon.nvim", 
+    priority = 1000 , 
+    config = true, 
+    opts = {},  
+  }, 
+    { "rebelot/kanagawa.nvim", 
+    priority = 1000 , 
+    config = true, 
+    opts = {},  
+  }, 
+      { "rebelot/kanagawa.nvim", 
+    priority = 1000 , 
+    config = true, 
+    opts = {},  
+  }, 
+    { "EdenEast/nightfox.nvim", 
+    priority = 1000 , 
+    config = true, 
+    opts = {},  
+  }, 
+     { "EdenEast/nightfox.nvim", 
+    priority = 1000 , 
+    config = true, 
+    opts = {},  
+  }, 
+  
+     { "EdenEast/nightfox.nvim", 
+    priority = 1000 , 
+    config = true, 
+    opts = {},  
+  }, 
+  
+  { "rose-pine/neovim",
+    name = "rose-pine",
+    config = true, 
+    opts = {},  
+  }, 
+  {'decaycs/decay.nvim', 
+    as = 'decay'
+    config = true, 
+    opts = {},}
+  
+  
+  
+}
