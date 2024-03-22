3ef9e3bdf794e813d8c43ecafb50f186c78ea91b
Enigma1309 <94917588+Enigma1309@users.noreply.github.com>
2024-03-22 23:39:34 +0530

    Gamemode.sh temporary fix for Hyprland V0.37.1.1

--- Configs/.config/hyprdots/scripts/gamemode.sh
+++ Configs/.config/hyprdots/scripts/gamemode.sh
@@ -1,19 +1,21 @@
 #!/usr/bin/env sh
-HYPRGAMEMODE=$(hyprctl getoption animations:enabled | sed -n '2p' | awk '{print $2}')
+HYPRGAMEMODE=$(hyprctl getoption animations:enabled | sed -n '1p' | awk '{print $2}')
 
 # Waybar performance
 FILE="$HOME/.config/waybar/style.css"
 
 sed -i 's/\/\* \(.*animation:.*\) \*\//\1/g' $FILE
+sed -i 's/\/\* \(.*transition:.*\) \*\//\1/g' $FILE
 if [ $HYPRGAMEMODE = 1 ]; then
-    sed -i 's/^\(.*animation:.*\)$/\/\* \1 \*\//g' $FILE
+	sed -i 's/^\(.*animation:.*\)$/\/\* \1 \*\//g' $FILE
+	sed -i 's/^\(.*transition:.*\)$/\/\* \1 \*\//g' $FILE
 fi
 killall waybar
-waybar > /dev/null 2>&1 &
+waybar >/dev/null 2>&1 &
 
 # Hyprland performance
-if [ $HYPRGAMEMODE = 1 ] ; then
-    hyprctl --batch "\
+if [ $HYPRGAMEMODE = 1 ]; then
+	hyprctl --batch "\
         keyword animations:enabled 0;\
         keyword decoration:drop_shadow 0;\
         keyword decoration:blur:enabled 0;\
@@ -21,6 +23,7 @@ if [ $HYPRGAMEMODE = 1 ] ; then
         keyword general:gaps_out 0;\
         keyword general:border_size 1;\
         keyword decoration:rounding 0"
-    exit
+	exit
+else
+	hyprctl reload
 fi
-hyprctl reload
