d5df93982e547b5c2a3bab3733fba4330c3fd02f
tittu <prasanthrangan@rediffmail.com>
2024-03-28 11:52:01 +0530

    wallbash: enable auto/dark/light mode

--- Configs/.config/hyprdots/scripts/wallbashtoggle.sh
+++ Configs/.config/hyprdots/scripts/wallbashtoggle.sh
@@ -1,18 +1,31 @@
 #!/usr/bin/env sh
 
+# lock instance
+lockFile="/tmp/hyrpdots$(id -u)swwwallpaper.lock"
+[ -e "$lockFile" ] && echo "An instance of the script is already running..." && exit 1
+touch "${lockFile}"
+trap 'rm -f ${lockFile}' EXIT
+
 # set variables
 ScrDir=`dirname "$(realpath "$0")"`
 source $ScrDir/globalcontrol.sh
 TgtScr="$ScrDir/globalcontrol.sh"
 
 # switch WallDcol variable
-if [ $EnableWallDcol -eq 1 ] ; then
-    sed -i "/^EnableWallDcol/c\EnableWallDcol=0" "${TgtScr}"
-    notif=" Wallbash disabled..."
-else
-    sed -i "/^EnableWallDcol/c\EnableWallDcol=1" "${TgtScr}"
-    notif=" Wallbash enabled..."
-fi
+case "${EnableWallDcol}" in
+    0)  sed -i "/^EnableWallDcol/c\EnableWallDcol=1" "${TgtScr}"
+        notif="wallbash auto"
+        ;;
+    1)  sed -i "/^EnableWallDcol/c\EnableWallDcol=2" "${TgtScr}"
+        notif="wallbash dark mode"
+        ;;
+    2)  sed -i "/^EnableWallDcol/c\EnableWallDcol=3" "${TgtScr}"
+        notif="wallbash light mode"
+        ;;
+    3)  sed -i "/^EnableWallDcol/c\EnableWallDcol=0" "${TgtScr}"
+        notif="wallbash disabled"
+        ;;
+esac
 
 # reset the colors
 "${ScrDir}/swwwallbash.sh"
