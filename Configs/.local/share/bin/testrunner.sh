9bf481868907989e4a7fdaeae90bbb0143aaf55b
tittu <prasanthrangan@rediffmail.com>
2024-04-03 01:35:21 +0530

    set as wallpaper menu fix

--- Configs/.local/share/bin/testrunner.sh
+++ Configs/.local/share/bin/testrunner.sh
@@ -2,90 +2,26 @@
 
 scrDir=`dirname "$(realpath "$0")"`
 source $scrDir/globalcontrol.sh
-WalDir="${XDG_CONFIG_HOME:-$HOME/.config}/swww"
-RofDir="${XDG_CONFIG_HOME:-$HOME/.config}/rofi"
+rofDir="${XDG_CONFIG_HOME:-$HOME/.config}/rofi"
 
-roficn=0
-wlogcn=1
+if [ "${1}" == "--verbose" ] || [ "${1}" == "-v" ] ; then
 
-while read loop_theme
-do
-    themeName=`echo $loop_theme | cut -d '|' -f 2`
-    $scrDir/themeswitch.sh -s $themeName &> /dev/null
-    sleep 0.2
+    case ${enableWallDcol} in
+        0) wallbashStatus="disabled";;
+        1) wallbashStatus="enabled // auto change based on wallpaper brightness";;
+        2) wallbashStatus="enabled // dark mode --forced";;
+        3) wallbashStatus="enabled // light mode --forced";;
+    esac
 
-    hyprctl dispatch workspace empty
-    dolphin &> /dev/null &
-    sleep 0.21
-    kitty &> /dev/null &
-    sleep 1.4
-    hyprctl dispatch workspace empty
+    echo -e "\n\ncurrent theme :: \"${hydeTheme}\" :: \"$(readlink "${hydeThemeDir}/wall.set")\""
+    echo -e "wallbash status :: ${enableWallDcol} :: ${wallbashStatus}\n"
+    get_themes
 
-    #walln=`ls -l $WalDir/$themeName | wc -l`
-    for (( i=1 ; i<3 ; i++ ))
-    do
-        # swww
-        sleep 0.2
-        $scrDir/swwwallpaper.sh -n &> /dev/null
-
-        # rofiselect
-        $scrDir/rofiselect.sh &> /dev/null &
-        sleep 0.7
-        pkill -x rofi
-
-        # rofi
-        if [ $roficn -lt 8 ] ; then
-            roficn=$(( roficn + 1 ))
-        else
-            roficn=1
-        fi
-        cp $RofDir/styles/style_$roficn.rasi $RofDir/config.rasi
-        $scrDir/rofilaunch.sh &> /dev/null &
-        sleep 0.7
-        pkill -x rofi
-
-        # themeselect
-        $scrDir/themeselect.sh &> /dev/null &
-        sleep 0.7
-        pkill -x rofi
-
-        # wlogout
-        if [ $wlogcn -eq 1 ] ; then
-            wlogcn=2
-        else
-            wlogcn=1
-        fi
-        $scrDir/logoutlaunch.sh $wlogcn &> /dev/null &
-        sleep 0.7
-        pkill -x wlogout
-
-        # waybar
-        $scrDir/wbarconfgen.sh n &> /dev/null
-
-        # quickapps
-        $scrDir/quickapps.sh kitty firefox spotify code dolphin &> /dev/null &
-        sleep 0.7
-        pkill -x rofi
-
-        # cliphist
-        $scrDir/cliphist.sh w &> /dev/null &
-        sleep 0.7
-        pkill -x rofi
-
-        # wallselect
-        $scrDir/swwwallselect.sh &> /dev/null &
-        sleep 0.7
-        pkill -x rofi
-
-        # wallbash
-        $scrDir/wallbashtoggle.sh
-        sleep 0.2
-
-        # volumecontrol
-        for (( i=1 ; i<=6 ; i++ )) ; do
-            [[ i -gt 3 ]] && vol="d" || vol="i"
-            $scrDir/volumecontrol.sh -o $vol
-        done
+    for x in "${!thmList[@]}" ; do
+        echo -e "\nTheme $((x + 1)) :: \${thmList[${x}]}=\"${thmList[x]}\" :: \${thmWall[${x}]}=\"${thmWall[x]}\"\n"
+        get_hashmap "$(dirname "${hydeThemeDir}")/${thmList[x]}" --verbose
+        echo -e "\n"
     done
-done < "$themeCtl"
 
+    exit 0
+fi
