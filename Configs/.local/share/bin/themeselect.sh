7147a7b1cbaaab20d802c1eea657f00b12eb08dd
tittu <prasanthrangan@rediffmail.com>
2024-04-05 12:23:58 +0530

    addng new theme select style 2

--- Configs/.local/share/bin/themeselect.sh
+++ Configs/.local/share/bin/themeselect.sh
@@ -12,14 +12,23 @@ rofiConf="${confDir}/rofi/themeselect.rasi"
 
 x_monres=$(hyprctl -j monitors | jq '.[] | select(.focused==true) | .width')
 monitor_scale=$(hyprctl -j monitors | jq '.[] | select (.focused == true) | .scale' | sed 's/\.//')
-x_monres=$(( x_monres * 17 / monitor_scale ))
 
 
 #// set rofi override
 
 elem_border=$(( hypr_border * 5 ))
 icon_border=$(( elem_border - 5 ))
-r_override="element{border-radius:${elem_border}px;} element-icon{border-radius:${icon_border}px;size:${x_monres}px;}"
+
+case "${themeSelect}" in
+2) # adapt to style 2
+    x_monres=$(( x_monres * 14 / monitor_scale ))
+    r_override="mainbox{background-color:#00000003;} listview{columns:4;} element{border-radius:${elem_border}px;background-color: @main-bg;padding:0em;} element-icon{border-radius:${icon_border}px 0px 0px ${icon_border}px;size:${x_monres}px;}"
+    thmbExtn="quad" ;;
+*) # default to style 1
+    x_monres=$(( x_monres * 17 / monitor_scale ))
+    r_override="element{border-radius:${elem_border}px;} element-icon{border-radius:${icon_border}px;size:${x_monres}px;}"
+    thmbExtn="sqre" ;;
+esac
 
 
 #// launch rofi menu
@@ -28,7 +37,7 @@ get_themes
 
 rofiSel=$(for i in ${!thmList[@]} ; do
     thmHash="$("${hashMech}" "${thmWall[i]}" | awk '{print $1}')"
-    echo -en "${thmList[i]}\x00icon\x1f"${thmbDir}/${thmHash}.sqre"\n"
+    echo -en "${thmList[i]}\x00icon\x1f"${thmbDir}/${thmHash}.${thmbExtn}"\n"
 done | rofi -dmenu -theme-str "${r_override}" -config "${rofiConf}" -select "${hydeTheme}")
 
 
