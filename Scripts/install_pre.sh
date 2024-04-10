a996b0a5c68c8e3e74e4b9659206ae8a410af1b9
Skerse <ajnelson086@proton.me>
2024-04-10 02:50:04 -0700

    Refactor: scripts & lists for spelling and formatting

--- Scripts/install_pre.sh
+++ Scripts/install_pre.sh
@@ -6,25 +6,21 @@
 
 scrDir=$(dirname "$(realpath "$0")")
 source "${scrDir}/global_fn.sh"
-if [ $? -ne 0 ] ; then
+if [ $? -ne 0 ]; then
     echo "Error: unable to source global_fn.sh..."
     exit 1
 fi
 
-
 # grub
-if pkg_installed grub && [ -f /boot/grub/grub.cfg ]
-    then
+if pkg_installed grub && [ -f /boot/grub/grub.cfg ]; then
     echo -e "\033[0;32m[BOOTLOADER]\033[0m detected // grub"
 
-    if [ ! -f /etc/default/grub.t2.bkp ] && [ ! -f /boot/grub/grub.t2.bkp ]
-        then
+    if [ ! -f /etc/default/grub.t2.bkp ] && [ ! -f /boot/grub/grub.t2.bkp ]; then
         echo -e "\033[0;32m[BOOTLOADER]\033[0m configuring grub..."
         sudo cp /etc/default/grub /etc/default/grub.t2.bkp
         sudo cp /boot/grub/grub.cfg /boot/grub/grub.t2.bkp
 
-        if nvidia_detect
-            then
+        if nvidia_detect; then
             echo -e "\033[0;32m[BOOTLOADER]\033[0m nvidia detected, adding nvidia_drm.modeset=1 to boot option..."
             gcld=$(grep "^GRUB_CMDLINE_LINUX_DEFAULT=" "/etc/default/grub" | cut -d'"' -f2 | sed 's/\b nvidia_drm.modeset=.\b//g')
             sudo sed -i "/^GRUB_CMDLINE_LINUX_DEFAULT=/c\GRUB_CMDLINE_LINUX_DEFAULT=\"${gcld} nvidia_drm.modeset=1\"" /etc/default/grub
@@ -33,12 +29,12 @@ if pkg_installed grub && [ -f /boot/grub/grub.cfg ]
         echo -e "Select grub theme:\n[1] Retroboot (dark)\n[2] Pochita (light)"
         read -p " :: Press enter to skip grub theme <or> Enter option number : " grubopt
         case ${grubopt} in
-            1) grubtheme="Retroboot";;
-            2) grubtheme="Pochita";;
-            *) grubtheme="None";;
+            1) grubtheme="Retroboot" ;;
+            2) grubtheme="Pochita" ;;
+            *) grubtheme="None" ;;
         esac
 
-        if [ "${grubtheme}" == "None" ] ; then
+        if [ "${grubtheme}" == "None" ]; then
             echo -e "\033[0;32m[BOOTLOADER]\033[0m Skippinng grub theme..."
             sudo sed -i "s/^GRUB_THEME=/#GRUB_THEME=/g" /etc/default/grub
         else
@@ -57,17 +53,13 @@ if pkg_installed grub && [ -f /boot/grub/grub.cfg ]
     fi
 fi
 
-
 # systemd-boot
-if pkg_installed systemd && nvidia_detect && [ $(bootctl status 2> /dev/null | awk '{if ($1 == "Product:") print $2}') == "systemd-boot" ]
-    then
+if pkg_installed systemd && nvidia_detect && [ $(bootctl status 2> /dev/null | awk '{if ($1 == "Product:") print $2}') == "systemd-boot" ]; then
     echo -e "\033[0;32m[BOOTLOADER]\033[0m detected // systemd-boot"
 
-    if [ $(ls -l /boot/loader/entries/*.conf.t2.bkp 2> /dev/null | wc -l) -ne $(ls -l /boot/loader/entries/*.conf 2> /dev/null | wc -l) ]
-        then
+    if [ $(ls -l /boot/loader/entries/*.conf.t2.bkp 2> /dev/null | wc -l) -ne $(ls -l /boot/loader/entries/*.conf 2> /dev/null | wc -l) ]; then
         echo "nvidia detected, adding nvidia_drm.modeset=1 to boot option..."
-        find /boot/loader/entries/ -type f -name "*.conf" | while read imgconf
-        do
+        find /boot/loader/entries/ -type f -name "*.conf" | while read imgconf; do
             sudo cp ${imgconf} ${imgconf}.t2.bkp
             sdopt=$(grep -w "^options" ${imgconf} | sed 's/\b quiet\b//g' | sed 's/\b splash\b//g' | sed 's/\b nvidia_drm.modeset=.\b//g')
             sudo sed -i "/^options/c${sdopt} quiet splash nvidia_drm.modeset=1" ${imgconf}
@@ -77,10 +69,8 @@ if pkg_installed systemd && nvidia_detect && [ $(bootctl status 2> /dev/null | a
     fi
 fi
 
-
 # pacman
-if [ -f /etc/pacman.conf ] && [ ! -f /etc/pacman.conf.t2.bkp ]
-    then
+if [ -f /etc/pacman.conf ] && [ ! -f /etc/pacman.conf.t2.bkp ]; then
     echo -e "\033[0;32m[PACMAN]\033[0m adding extra spice to pacman..."
 
     sudo cp /etc/pacman.conf /etc/pacman.conf.t2.bkp
@@ -89,15 +79,9 @@ if [ -f /etc/pacman.conf ] && [ ! -f /etc/pacman.conf.t2.bkp ]
     /^#ParallelDownloads/c\ParallelDownloads = 5" /etc/pacman.conf
     sudo sed -i '/^#\[multilib\]/,+1 s/^#//' /etc/pacman.conf
 
-    #if [ $(grep -w "^\[xero_hypr\]" /etc/pacman.conf | wc -l) -eq 0 ] && [ $(grep "https://repos.xerolinux.xyz/xero_hypr/x86_64/" /etc/pacman.conf | wc -l) -eq 0 ]
-    #    then
-    #    echo "adding [xero_hypr] repo to pacman..."
-    #    echo -e "\n[xero_hypr]\nSigLevel = Required DatabaseOptional\nServer = https://repos.xerolinux.xyz/xero_hypr/x86_64/\n\n" | sudo tee -a /etc/pacman.conf
-    #fi
     sudo pacman -Syyu
     sudo pacman -Fy
 
 else
     echo -e "\033[0;33m[SKIP]\033[0m pacman is already configured..."
 fi
-
