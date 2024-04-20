7bb3e2b3ea326f3f9d0c3fd45ed1fd91a890b01e
tittu <prasanthrangan@rediffmail.com>
2024-04-20 15:29:56 +0530

    simplify clipboard logic

--- Configs/.local/share/bin/cliphist.sh
+++ Configs/.local/share/bin/cliphist.sh
@@ -1,96 +1,67 @@
 #!/usr/bin/env sh
 
+
+#// set variables
+
 scrDir=$(dirname "$(realpath "$0")")
 source $scrDir/globalcontrol.sh
 roconf="${confDir}/rofi/clipboard.rasi"
 
+
+#// set rofi scaling
+
 [[ "${rofiScale}" =~ ^[0-9]+$ ]] || rofiScale=10
-
-# TODO : Find a smarter way to do this; sol for rofi clipboard size
-clp_file=$cacheDir/landing/clip.size
-[ -f "${clp_file}" ] && . "${clp_file}"
-clip_size() {
-    sleep 0.4
-    eval "$(hyprctl layers -j | jq -r --arg mon "$monName" '.[$mon].levels | .[][] | select(.namespace == "rofi") | "export w_clip=\(.w); export h_clip=\(.h)"')"
-    { [ "${w_clip}" != "${wBoard}" ] || [ "${h_clip}" != "${hBoard}" ]; } && echo "wBoard=${w_clip}; hBoard=${h_clip}" >"${clp_file}"
-}
-
-# set position
-x_offset=${clipXoffset:-15}  #* Cursor spawn position on clipboard
-y_offset=${clipYoffset:-210} #* To point the Cursor to the 1st and 2nd latest word
-
-# Monitor resolution , scale and rotation
-eval "$(hyprctl monitors -j | jq -r \
-    ' .[] | select(.focused==true) | 
- (if (.transform | (. % 2) == 1) then
-   {monWidth: (.height / .scale | floor), monHeight: (.width / .scale | floor)}
- else
-   {monWidth: (.width / .scale | floor), monHeight: (.height / .scale | floor)}
- end) as $dims |
-"export monName=\(.name);
-export monWidth=\($dims.monWidth);
-export monHeight=\($dims.monHeight);
-export monXpos=\(.x | floor);
-export monYpos=\(.y | floor);
-"')"
-
-# Level 1 layers  e.g namespace for  waybar, for now just waybar \\ ensures that we can get a good boundary value
-# TODO Handle if we got multple waybar location
-wBar=0
-hBar=0
-eval "$(hyprctl layers -j | jq -r --arg mon "$monName" '.[$mon].levels | .[] | .[] | select(.namespace == "waybar") | if .h < .w then "export export hBar=$((\(.h) + $hBar ))" else "export wBar=$((\(.w) + $wBar ))" end')"
-
-#  Cursor position filtered by Monitor stats
-eval "$(hyprctl cursorpos -j | jq -r \
-    --argjson x_offset "$x_offset" \
-    --argjson y_offset "$y_offset" \
-    --argjson monXpos "$monXpos" \
-    --argjson monYpos "$monYpos" \
-    '"
-export curXpos=\(.x - $monXpos - $x_offset )
-export curYpos=\(.y - $monYpos - $y_offset)
-"')"
-
-# Limit board location to monitor
-xBound=$((monWidth - wBoard - wBar))
-yBound=$((monHeight - hBoard - hBar))
-curXpos=$((curXpos - x_offset))
-curYpos=$((curYpos - y_offset))
-
-curXpos=$((curXpos < 0 ? 0 : (curXpos > xBound ? xBound : curXpos)))
-curYpos=$((curYpos < 0 ? 0 : (curYpos > yBound ? yBound : curYpos)))
-
-# position override
-pos="window {location: north west; x-offset: ${curXpos}px; y-offset: ${curYpos}px;}"
-
-# read hypr theme border
+r_scale="configuration {font: \"JetBrainsMono Nerd Font ${rofiScale}\";}"
 wind_border=$((hypr_border * 3 / 2))
 elem_border=$([ $hypr_border -eq 0 ] && echo "5" || echo $hypr_border)
-r_override="window {border: ${hypr_width}px; border-radius: ${wind_border}px;} entry {border-radius: ${elem_border}px;} element {border-radius: ${elem_border}px;}"
 
-# read hypr font size
-fnt_override="configuration {font: \"JetBrainsMono Nerd Font ${rofiScale}\";}"
 
-# clipboard action
-case $1 in
-c)
-    (cliphist list | rofi -dmenu -theme-str "entry { placeholder: \"Copy...\";} ${pos} ${r_override}" -theme-str "${fnt_override}" -config $roconf | cliphist decode | wl-copy) &
-    clip_size
+#// evaluate spawn position
+
+readarray -t curPos < <(hyprctl cursorpos -j | jq -r '.x,.y')
+readarray -t monRes < <(hyprctl -j monitors | jq '.[] | select(.focused==true) | .width,.height')
+readarray -t layRes < <(hyprctl -j monitors | jq -r '.[] | select(.focused==true).reserved | map(tostring) | join("\n")')
+
+if [ "${curPos[0]}" -ge "$((${monRes[0]} / 2))" ] ; then
+    x_pos="east"
+    x_lay=${monRes[0]}
+    x_off="-$(( ${monRes[0]} - ${curPos[0]} - ${layRes[2]} ))"
+else
+    x_pos="west"
+    x_off="$(( ${curPos[0]} - ${layRes[0]} ))"
+fi
+
+if [ "${curPos[1]}" -ge "$((${monRes[1]} / 2))" ] ; then
+    y_pos="south"
+    y_off="-$(( ${monRes[1]} - ${curPos[1]} - ${layRes[3]} ))"
+else
+    y_pos="north"
+    y_off="$(( ${curPos[1]} - ${layRes[1]} ))"
+fi
+
+pos_override="window{location:${x_pos} ${y_pos};anchor:${x_pos} ${y_pos};x-offset:${x_off}px;y-offset:${y_off}px;border:${hypr_width}px;border-radius:${wind_border}px;} wallbox{border-radius:${elem_border}px;} element{border-radius:${elem_border}px;}"
+
+
+#// clipboard action
+
+case "${1}" in
+c|-c|--copy)
+    cliphist list | rofi -dmenu -theme-str "entry { placeholder: \"Copy...\";}" -theme-str "${r_scale}" -theme-str "${pos_override}" -config "${roconf}" | cliphist decode | wl-copy
     ;;
-d)
-    (cliphist list | rofi -dmenu -theme-str "entry { placeholder: \"Delete...\";} ${pos} ${r_override}" -theme-str "${fnt_override}" -config $roconf | cliphist delete) &
-    clip_size
+d|-d|--delete)
+    cliphist list | rofi -dmenu -theme-str "entry { placeholder: \"Delete...\";}" -theme-str "${r_scale}" -theme-str "${pos_override}" -config "${roconf}" | cliphist delete
     ;;
-w)
-    if [ $(echo -e "Yes\nNo" | rofi -dmenu -theme-str "entry { placeholder: \"Clear Clipboard History?\";} ${pos} ${r_override}" -theme-str "${fnt_override}" -config $roconf) == "Yes" ]; then
+w|-w|--wipe)
+    if [ $(echo -e "Yes\nNo" | rofi -dmenu -theme-str "entry { placeholder: \"Clear Clipboard History?\";}" -theme-str "${r_scale}" -theme-str "${pos_override}" -config "${roconf}") == "Yes" ] ; then
         cliphist wipe
     fi
     ;;
 *)
     echo -e "cliphist.sh [action]"
-    echo "c :  cliphist list and copy selected"
-    echo "d :  cliphist list and delete selected"
-    echo "w :  cliphist wipe database"
+    echo "c -c --copy    :  cliphist list and copy selected"
+    echo "d -d --delete  :  cliphist list and delete selected"
+    echo "w -w --wipe    :  cliphist wipe database"
     exit 1
     ;;
 esac
+
