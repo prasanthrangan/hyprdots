18cbf06aa1d7eaf9a940eeff6d1dd0b09283cabf
Mahaveer Gurjar <mahaveergurjar.work@gmail.com>
2024-04-22 12:21:53 +0530

    Fix brightness icon for notification

--- Configs/.local/share/bin/brightnesscontrol.sh
+++ Configs/.local/share/bin/brightnesscontrol.sh
@@ -17,7 +17,7 @@ function send_notification {
     brightness=`brightnessctl info | grep -oP "(?<=\()\d+(?=%)" | cat`
     brightinfo=$(brightnessctl info | awk -F "'" '/Device/ {print $2}')
     angle="$(((($brightness + 2) / 5) * 5))"
-    ico="~/.config/dunst/icons/vol/vol-${angle}.svg"
+    ico="$HOME/.config/dunst/icons/vol/vol-${angle}.svg"
     bar=$(seq -s "." $(($brightness / 15)) | sed 's/[0-9]//g')
     notify-send -a "t2" -r 91190 -t 800 -i "${ico}" "${brightness}${bar}" "${brightinfo}"
 }
