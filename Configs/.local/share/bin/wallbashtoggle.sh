e3a07cc30f02cd7af64c64c082ca41f2766b24c2
Mahaveer Gurjar <mahaveergurjar.work@gmail.com>
2024-04-23 20:48:16 +0530

    Fix notification icons problem (#1364)
    * Fix brightness icon for notification
    
    * Fix notification issue
    
    * fix notification issue
    
    * fix notification issue

--- Configs/.local/share/bin/wallbashtoggle.sh
+++ Configs/.local/share/bin/wallbashtoggle.sh
@@ -57,5 +57,5 @@ export reload_flag=1
 [[ "${setMode}" -lt 0 ]] && setMode=$((${#wallbashModes[@]} - 1))
 set_conf "enableWallDcol" "${setMode}"
 "${scrDir}/swwwallpaper.sh" -s "$(readlink "${hydeThemeDir}/wall.set")"
-notify-send -a "t1" -i "~/.config/dunst/icons/hyprdots.png" " ${wallbashModes[setMode]} mode"
+notify-send -a "t1" -i "$HOME/.config/dunst/icons/hyprdots.png" " ${wallbashModes[setMode]} mode"
 
