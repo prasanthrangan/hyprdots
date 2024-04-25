5fa89f4675549c352d42207a25ea010f1cf1b3f0
kRHYME7 <alampayk@gmail.com>
2024-04-25 19:35:24 +0800

    layouts 1 2 3

--- Configs/.local/share/bin/hyprlock.sh
+++ Configs/.local/share/bin/hyprlock.sh
@@ -4,27 +4,33 @@
 ScrDir=$(dirname "$(realpath "$0")")
 source ${ScrDir}/globalcontrol.sh
 
-WALL="${cacheDir}/wall.set"
+WALLPAPER="${cacheDir}/wall.set"
 
 fn_background() {
-    SL="${confDir}/hyprlock/hyprlock.png"
-    mime=$(file --mime-type "${WALL}" | grep "image/png")
-    cp -f "${WALL}" "${SL}"
+    BG="${confDir}/hyprlock/hyprlock.png"
+    mime=$(file --mime-type "${WALLPAPER}" | grep "image/png")
+    cp -f "${WALLPAPER}" "${BG}"
     #? Run this in the background because converting takes time
-    ([[ -z ${mime} ]] && convert "${SL}"[0] "${SL}") &
+    ([[ -z ${mime} ]] && convert "${BG}"[0] "${BG}") &
 }
 
-mpris_thumb() {
+mpris_thumb() { # Generate thumbnail for mpris
     artUrl=$(playerctl -p spotify metadata --format '{{mpris:artUrl}}')
-    [ "${artUrl}" == "$(cat "${THUMB}".lnk)" ] && exit 0
+    [ "${artUrl}" == "$(cat "${THUMB}".lnk)" ] && [ -f "${THUMB}".png ] && exit 0
     echo "${artUrl}" > "${THUMB}".lnk
     curl -Lso "${THUMB}".art "$artUrl"
-    convert "${THUMB}".art -quality 50 "${THUMB}".png
+    convert "${THUMB}.art" -quality 50 "${THUMB}.png"
     pkill -USR2 hyprlock # updates the mpris thumbnail
 }
+
 fn_mpris() {
     THUMB="${cacheDir}/landing/mpris"
-    { playerctl -p spotify metadata --format '{{title}}    {{artist}}' && mpris_thumb; } || { rm -f "${THUMB}".png && exit 0; }
+    if [ "$(playerctl -p spotify status)" != "Paused" ]; then
+        playerctl -p spotify metadata --format '{{title}}    {{artist}}'  
+        mpris_thumb
+    else :
+        rm -f "${THUMB}".png && exit 0
+    fi
 }
 
 fn_cava() {
