ea59d661e30d020ba4e16ac7b033b906322d0402
Rubin Bhandari <roobin.bhandari@gmail.com>
2024-04-27 00:00:57 +0545

    feat: add more alias (#1359)
    * feat: add more alias
    
    * feat(fish): add more alias
    
    * vscode wallbash integration
    
    * Replaced neofetch with fastfetch in systemupdate.sh
    
    HyDE has transitioned to using fastfetch rather than neofetch by default.
    
    This change allows the systemupdate.sh script to execute correctly on a system that does not have the 'neofetch' package.
    
    * adding showcase video
    
    * fix(kityy): remove kitty ssh alias
    
    This causes issues when you use terminals other than kitty,
    so its best to not hardcode it as default
    
    * Discard changes to Configs/.config/hyde/wallbash/Wall-Ways/code.dcol
    
    ---------
    
    Co-authored-by: tittu <prasanthrangan@rediffmail.com>
    Co-authored-by: Nate <119661777+gitnatel@users.noreply.github.com>
    Co-authored-by: Tittu <106020512+prasanthrangan@users.noreply.github.com>

--- Configs/.config/fish/config.fish
+++ Configs/.config/fish/config.fish
@@ -20,6 +20,3 @@ abbr .5 'cd ../../../../..'
 
 # Always mkdir a path (this doesn't inhibit functionality to make a single dir)
 abbr mkdir 'mkdir -p'
-
-# Fixes "Error opening terminal: xterm-kitty" when using the default kitty term to open some programs through ssh
-alias ssh='kitten ssh'
