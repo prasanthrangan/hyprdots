325a06dfa1c42474f68a2d988a42df3ec2581075
kRHYME7 <alampayk@gmail.com>
2024-04-29 12:44:02 +0800

    Fixes some ENV block errors and ensure shell are run inside hyprland ENV

--- Configs/.local/share/bin/wallbashqt.sh
+++ Configs/.local/share/bin/wallbashqt.sh
@@ -18,6 +18,6 @@ a_ws=$(hyprctl -j activeworkspace | jq '.id')
 dpid=$(hyprctl -j clients | jq --arg wid "$a_ws" '.[] | select(.workspace.id == ($wid | tonumber)) | select(.class == "org.kde.dolphin") | .pid')
 if [ ! -z ${dpid} ] ; then
     hyprctl dispatch closewindow pid:${dpid}
-    dolphin &
+    hyprctl dispatch exec dolphin &
 fi
 
