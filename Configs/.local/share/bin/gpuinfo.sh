df82cf1d6faa9c98118781e89c5f4ba3ea63d2aa
Skerse <ajnelson086@proton.me>
2024-05-06 19:32:53 -0700

    refactor: more on the gpuinfo script

--- Configs/.local/share/bin/gpuinfo.sh
+++ Configs/.local/share/bin/gpuinfo.sh
@@ -12,8 +12,7 @@ if [[ ! " $* " =~ " startup " ]]; then
    gpuQ="${gpuQ}$2"
 fi
 
-detect() {
-    # Auto detect Gpu used by Hyprland(declared using env = WLR_DRM_DEVICES) Sophisticated?
+detect() { # Auto-detect GPU used by Hyprland
     card=$(echo "${WLR_DRM_DEVICES}" | cut -d':' -f1 | cut -d'/' -f4)
     # shellcheck disable=SC2010
     slot_number=$(ls -l /dev/dri/by-path/ | grep "${card}" | awk -F'pci-0000:|-card' '{print $2}')
@@ -41,8 +40,8 @@ query() {
         echo "nvidia_flag=1 # Using nouveau an open-source nvidia driver" >>"${gpuQ}"
     elif command -v nvidia-smi &>/dev/null; then
         nvidia_gpu=$(nvidia-smi --query-gpu=gpu_name --format=csv,noheader,nounits | head -n 1)
-        if [[ -n "${nvidia_gpu}" ]]; then  # Check for NVIDIA GPU
-            if [[ "${nvidia_gpu}" == *"NVIDIA-SMI has failed"* ]]; then  #? Second Layer for dGPU
+        if [[ -n "${nvidia_gpu}" ]]; then # Check for Nvidia GPU
+            if [[ "${nvidia_gpu}" == *"NVIDIA-SMI has failed"* ]]; then #? Second layer for dGPU
                 echo "nvidia_flag=0 # NVIDIA-SMI has failed" >> "${gpuQ}"
             else
                 nvidia_address=$(lspci | grep -Ei "VGA|3D" | grep -i "${nvidia_gpu/NVIDIA /}" | cut -d' ' -f1)
@@ -70,7 +69,7 @@ query() {
         intel_address=$(lspci | grep -Ei "VGA|3D" | grep -i "${intel_gpu}" | cut -d' ' -f1)
         {
             echo "intel_address=\"${intel_address}\""
-            echo "intel_flag=1"  # Check for Intel GPU
+            echo "intel_flag=1" # Check for Intel GPU
             echo "intel_gpu=\"${intel_gpu}\""
         } >>"${gpuQ}"
     fi
@@ -84,7 +83,7 @@ toggle() {
     if [[ -n "$1" ]]; then
         next_prioGPU="$1_flag"
     else
-        # Initialize gpu_flags and prioGPU if they don't exist
+        # Initialize gpu_flags and priority GPU if they don't exist
         if ! grep -q "gpu_flags=" "${gpuQ}"; then
             gpu_flags=$(grep "flag=1" "${gpuQ}" | cut -d '=' -f 1 | tr '\n' ' ' | tr -d '#')
             echo "" >> "${gpuQ}"
@@ -97,8 +96,8 @@ toggle() {
             echo "prioGPU=${initGPU}" >> "${gpuQ}"
         fi
         mapfile -t anchor < <(grep "flag=1" "${gpuQ}" | cut -d '=' -f 1)
-        prioGPU=$(grep "prioGPU=" "${gpuQ}" | cut -d'=' -f 2)    # Get the current prioGPU from the file
-        # Find the index of the current prioGPU in the anchor array
+        prioGPU=$(grep "prioGPU=" "${gpuQ}" | cut -d'=' -f 2) # Get the current priority GPU from the file
+        # Find the index of the current priority GPU in the anchor array
         for index in "${!anchor[@]}"; do
             if [[ "${anchor[${index}]}" = "${prioGPU}" ]]; then
                 current_index=${index}
@@ -108,12 +107,14 @@ toggle() {
         next_prioGPU=${anchor[${next_index}]#\#}
     fi
 
-    # Set the next prioGPU and remove the '#' character
-    sed -i 's/^\(nvidia_flag=1\|amd_flag=1\|intel_flag=1\)/#\1/' "${gpuQ}" # Comment out all the gpu flags in the file
-    sed -i "s/^#${next_prioGPU}/${next_prioGPU}/" "${gpuQ}" # Uncomment the next prioGPU in the file
-    sed -i "s/prioGPU=${prioGPU}/prioGPU=${next_prioGPU}/" "${gpuQ}" # Update the prioGPU in the file
+    # Set the next priority GPU and remove the '#' character
+    sed -i 's/^\(nvidia_flag=1\|amd_flag=1\|intel_flag=1\)/#\1/' "${gpuQ}" # Comment out all the GPU flags in the file
+    sed -i "s/^#${next_prioGPU}/${next_prioGPU}/" "${gpuQ}" # Uncomment the next priority GPU in the file
+    sed -i "s/prioGPU=${prioGPU}/prioGPU=${next_prioGPU}/" "${gpuQ}" # Update the priority GPU in the file
 }
 
+# Thee shalt find the greatest one,
+# He who not more than the chosen one
 map_floor() {
     # From the depths of the string, words arise,
     # Keys in pairs, a treasure in disguise.
@@ -144,8 +145,7 @@ map_floor() {
     [ -n "$def_val" ] && echo $def_val || echo " "
 }
 
-get_icons() {
-    # Key-value pairs of temperature and utilization levels
+get_icons() { # Key-value pairs of temperature and utilization levels
     temp_lv="85:&🌋, 65:&🔥, 45:&☁️, &❄️"
     util_lv="90:, 60:󰓅, 30:󰾅, 󰾆"
 
@@ -155,8 +155,7 @@ get_icons() {
     echo $icons
 }
 
-generate_json() {
-    # Get emoji and icon based on temperature and utilization
+generate_json() { # Get emoji and icon based on temperature and utilization
     icons=$(get_icons "$temperature" "$utilization")
     thermo=$(echo $icons | awk -F, '{print $1}')
     emoji=$(echo $icons | awk -F, '{print $2}')
@@ -164,19 +163,30 @@ generate_json() {
 
     # emoji=$(get_temperature_emoji "${temperature}")
     local json="{\"text\":\"${thermo} ${temperature}°C\", \"tooltip\":\"${primary_gpu}\n${thermo} Temperature: ${temperature}°C ${emoji}"
-    #? Soon Add Something incase needed.
+    #? Soon add Something incase needed.
     declare -A tooltip_parts
-    if [[ -n "${utilization}" ]]; then tooltip_parts["\n$speedo Utilization: "]="${utilization}%" ; fi
-    if [[ -n "${current_clock_speed}" ]] && [[ -n "${max_clock_speed}" ]]; then tooltip_parts["\n Clock Speed: "]="${current_clock_speed}/${max_clock_speed} MHz" ; fi
-    if [[ -n "${gpu_load}" ]]; then tooltip_parts["\n$speedo Utilization: "]="${gpu_load}%" ; fi
-    if [[ -n "${core_clock}" ]]; then tooltip_parts["\n Clock Speed: "]="${core_clock} MHz" ;fi
-    if [[ -n "${power_usage}" ]]; then if [[ -n "${power_limit}" ]]; then
-        tooltip_parts["\n󱪉 Power Usage: "]="${power_usage}/${power_limit} W"
-    else
-        tooltip_parts["\n󱪉 Power Usage: "]="${power_usage} W"
+    if [[ -n "${utilization}" ]]; then
+        tooltip_parts["\n$speedo Utilization: "]="${utilization}%"
     fi
+    if [[ -n "${current_clock_speed}" ]] && [[ -n "${max_clock_speed}" ]]; then
+        tooltip_parts["\n Clock Speed: "]="${current_clock_speed}/${max_clock_speed} MHz"
     fi
-    if [[ -n "${power_discharge}" ]] && [[ "${power_discharge}" != "0" ]]; then tooltip_parts["\n Power Discharge: "]="${power_discharge} W" ;fi
+    if [[ -n "${gpu_load}" ]]; then
+        tooltip_parts["\n$speedo Utilization: "]="${gpu_load}%"
+    fi
+    if [[ -n "${core_clock}" ]]; then
+        tooltip_parts["\n Clock Speed: "]="${core_clock} MHz"
+    fi
+    if [[ -n "${power_usage}" ]]; then
+        if [[ -n "${power_limit}" ]]; then
+            tooltip_parts["\n󱪉 Power Usage: "]="${power_usage}/${power_limit} W"
+        else
+            tooltip_parts["\n󱪉 Power Usage: "]="${power_usage} W"
+        fi
+    fi
+    if [[ -n "${power_discharge}" ]] && [[ "${power_discharge}" != "0" ]]; then
+        tooltip_parts["\n Power Discharge: "]="${power_discharge} W"
+    fi
 
     for key in "${!tooltip_parts[@]}"; do
         local value="${tooltip_parts[${key}]}"
@@ -188,8 +198,7 @@ generate_json() {
     echo "${json}"
 }
 
-general_query() {
-    # Function to get temperature from 'sensors'
+general_query() { # Function to get temperature from 'sensors'
     filter=''
     temperature=$(sensors | ${filter} grep -m 1 -E "(edge|Package id.*|another keyword)" | awk -F ':' '{print int($2)}') #! We can get json data from sensors too
     # gpu_load=$()
@@ -206,17 +215,18 @@ general_query() {
     max_clock_speed=$(awk '{print $1/1000}' /sys/devices/system/cpu/cpu0/cpufreq/cpuinfo_max_freq)
 }
 
-intel_GPU() {
-    #? Function to query basic intel GPU
+intel_GPU() { #? Function to query basic Intel GPU
     primary_gpu="Intel ${intel_gpu}"
     general_query
 }
 
-nvidia_GPU() {
-    #? Function to query Nvidia GPU
+nvidia_GPU() { #? Function to query Nvidia GPU
     primary_gpu="NVIDIA ${nvidia_gpu}"
-    if [[ "${nvidia_gpu}" == "Linux" ]]; then general_query ; return ; fi #? Open source driver
-    #? Tired Flag for not using nvidia-smi if GPU is in suspend mode.
+    if [[ "${nvidia_gpu}" == "Linux" ]]; then
+        general_query
+        return
+    fi
+    #? Tired flag for not using nvidia-smi if GPU is in suspend mode.
     if ${tired}; then
         is_suspend="$(cat /sys/bus/pci/devices/0000:"${nvidia_address}"/power/runtime_status)"
         if [[ ${is_suspend} == *"suspend"* ]]; then
@@ -236,13 +246,12 @@ nvidia_GPU() {
     power_limit="${gpu_data[5]// /}"
 }
 
-amd_GPU() {
-    #? Funtion to query amd GPU
+amd_GPU() { #? Function to query AMD GPU
     primary_gpu="AMD ${amd_gpu}"
-    # Execute the AMD GPU Python script and use its output
+    # Execute the AMD GPU python script and use its output
     amd_output=$(python3 ${scrDir}/amdgpu.py)
     if [[ ! ${amd_output} == *"No AMD GPUs detected."* ]] && [[ ! ${amd_output} == *"Unknown query failure"* ]]; then
-        # Extract GPU Temperature, GPU Load, GPU Core Clock, and GPU Power Usage from amd_output
+        # Extract GPU temperature, GPU load, GPU core clock, and GPU power usage from amd_output
         temperature=$(echo "${amd_output}" | jq -r '.["GPU Temperature"]' | sed 's/°C//')
         gpu_load=$(echo "${amd_output}" | jq -r '.["GPU Load"]' | sed 's/%//')
         core_clock=$(echo "${amd_output}" | jq -r '.["GPU Core Clock"]' | sed 's/ GHz//;s/ MHz//')
@@ -298,7 +307,6 @@ nvidia_flag=${nvidia_flag:-0}
 intel_flag=${intel_flag:-0}
 amd_flag=${amd_flag:-0}
 
-#? Based on the flags, call the corresponding function multi flags means multi GPU.
 if [[ "${nvidia_flag}" -eq 1 ]]; then
     nvidia_GPU
 elif [[ "${amd_flag}" -eq 1 ]]; then
