2ac4ff5e9dbf02cc30c7b259279e2e58912e4d56
dieBakterie <50206827+dieBakterie@users.noreply.github.com>
2024-05-22 13:39:11 +0000

    Update wallbashdiscord.sh
    Enables Discord and derivatives Theme for disabled wallbash/mode Theme. Likely Buttons that are only have a border, for example "deactivate account" and "remove authentication-app" will have an invisible text or like "Edit User Profile", "Change Password", "View Backup Codes", "Enable SMS Authentication" and "Register a Security Key" in "Graphite Mono" Theme are some of the invisible buttons. All of those buttons have a blue background and white text and are unchanged as it seems.

--- Configs/.local/share/bin/wallbashdiscord.sh
+++ Configs/.local/share/bin/wallbashdiscord.sh
@@ -22,13 +22,8 @@ client_list+=("$HOME/.var/app/xyz.armcord.ArmCord/config/ArmCord/themes/theme.cs
 
 for client_css in "${client_list[@]}" ; do
     eval client_css="${client_css}"
-    if [[ ! -d $(dirname "${client_css}") ]] ; then
-        continue
-    fi
-    if [[ "${enableWallDcol}" -gt 0 ]] ; then
+    if [[  -d $(dirname "${client_css}") ]] ; then
         cp "${discord_col}" "${client_css}"
-    else
-        > "${client_css}"
     fi
 done
 
