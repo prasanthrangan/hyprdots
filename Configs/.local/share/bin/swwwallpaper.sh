af5573077254f88c4d9988f0f114149217d041cf
Rubin Bhandari <roobin.bhandari@gmail.com>
2024-07-02 20:30:14 +0545

    fix : swww fails when the hyprctl cursor do not give correct value, This PR will handle it using 0,0 as fallback

--- Configs/.local/share/bin/swwwallpaper.sh
+++ Configs/.local/share/bin/swwwallpaper.sh
@@ -98,7 +98,6 @@ done
 swww query &> /dev/null
 if [ $? -ne 0 ] ; then
     swww-daemon --format xrgb &
-    swww query && swww restore
 fi
 
 
@@ -112,5 +111,4 @@ fi
 #// apply wallpaper
 
 echo ":: applying wall :: \"$(readlink -f "${wallSet}")\""
-swww img "$(readlink "${wallSet}")" --transition-bezier .43,1.19,1,.4 --transition-type "${xtrans}" --transition-duration "${wallTransDuration}" --transition-fps "${wallFramerate}" --invert-y --transition-pos "$(hyprctl cursorpos)" &
-
+swww img "$(readlink "${wallSet}")" --transition-bezier .43,1.19,1,.4 --transition-type "${xtrans}" --transition-duration "${wallTransDuration}" --transition-fps "${wallFramerate}" --invert-y --transition-pos "$(hyprctl cursorpos | grep -E '^[0-9]' || echo "0,0")" &
