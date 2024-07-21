54e072e4a233644b8b84109eb772699be275610b
kRHYME7 <alampayk@gmail.com>
2024-07-21 09:52:48 +0800

    fix not reloading the color-scheme

--- Configs/.local/share/bin/swwwallbash.sh
+++ Configs/.local/share/bin/swwwallbash.sh
@@ -237,18 +237,21 @@ if [ "${enableWallDcol}" -eq 0 ] && [[ "${reload_flag}" -eq 1 ]] ; then
     done < <(find "${wallbashDir}/Wall-Dcol" -type f -name "*.dcol")
 
     parallel fn_wallbash ::: "${deployList[@]}"
-    # detects the color-scheme set in hypr.theme and falls back if nothing is parsed.
+elif [ "${enableWallDcol}" -gt 0 ] ; then
+
+    echo ":: deploying wallbash colors :: ${dcol_mode} wallpaper detected"
+    find "${wallbashDir}/Wall-Dcol" -type f -name "*.dcol" | parallel fn_wallbash {}
+
+fi
+
+#  Theme mode: detects the color-scheme set in hypr.theme and falls back if nothing is parsed.
+if [ "${enableWallDcol}" -eq 0 ]; then
     colorScheme="$({ grep -q "^[[:space:]]*\$COLOR-SCHEME\s*=" "${hydeThemeDir}/hypr.theme" && grep "^[[:space:]]*\$COLOR-SCHEME\s*=" "${hydeThemeDir}/hypr.theme" | cut -d '=' -f2 | sed 's/^[[:space:]]*//;s/[[:space:]]*$//' ;} || 
                     grep 'gsettings set org.gnome.desktop.interface color-scheme' "${hydeThemeDir}/hypr.theme" | awk -F "'" '{print $((NF - 1))}')"
     colorScheme=${colorScheme:-$(gsettings get org.gnome.desktop.interface color-scheme)} 
     # should be declared explicitly so we can easily debug
     grep -q "dark" <<< "${colorScheme}" && enableWallDcol=2
     grep -q "light" <<< "${colorScheme}" && enableWallDcol=3 
-elif [ "${enableWallDcol}" -gt 0 ] ; then
-
-    echo ":: deploying wallbash colors :: ${dcol_mode} wallpaper detected"
-    find "${wallbashDir}/Wall-Dcol" -type f -name "*.dcol" | parallel fn_wallbash {}
-
 fi
 
 find "${wallbashDir}/Wall-Ways" -type f -name "*.dcol" | parallel fn_wallbash {}
