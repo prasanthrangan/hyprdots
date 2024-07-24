66b54801f8f5ef1b8e0163f86e720fd1b2958670
Sanjay <102979855+sanjyay@users.noreply.github.com>
2024-07-24 22:49:40 +0530

    Update blue-light-filter.glsl

--- Configs/.config/hypr/shaders/blue-light-filter.glsl
+++ Configs/.config/hypr/shaders/blue-light-filter.glsl
@@ -1,4 +1,4 @@
-https://github.com/hyprwm/Hyprland/issues/1140#issuecomment-1335128437
+// https://github.com/hyprwm/Hyprland/issues/1140#issuecomment-1335128437
 precision highp float;
 varying vec2 v_texcoord;
 uniform sampler2D tex;
