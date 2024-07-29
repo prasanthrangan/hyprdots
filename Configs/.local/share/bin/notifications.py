3ab2ced8c2e9acbadcc8053caf6beadfd81e49be
Mahaveer Gurjar <mahaveergurjar.work@gmail.com>
2024-07-29 20:24:31 +0530

    Update notifications.py

--- Configs/.local/share/bin/notifications.py
+++ Configs/.local/share/bin/notifications.py
@@ -14,7 +14,7 @@ def format_history(history):
     alt = 'none'
     tooltip_click = []
     tooltip_click.append("󰎟 Notifications")
-    tooltip_click.append("󰳽 click-left:  history pop")
+    tooltip_click.append("󰳽 click-left:  Enable & Disable DND")
     tooltip_click.append("󰳽 click-middle: 󰛌 clear history")
     tooltip_click.append("󰳽 click-right: 󱄊 close all")
 
