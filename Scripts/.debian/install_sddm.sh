b375c8129ca964e265a71fcc36e52546020b3c18
iiztp <41050796+iiztp@users.noreply.github.com>
2024-09-13 00:31:18 +0200

    Create install_sddm.sh

---
+++ Scripts/.debian/install_sddm.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+#|---/ /+----------------------------------------+---/ /|#
+#|--/ /-| Script to install sddm                 |--/ /-|#
+#|-/ /--| Matthieu Amet                          |-/ /--|#
+#|-/ /--| Prasanth Rangan                        |-/ /--|#
+#|/ /---+----------------------------------------+/ /---|#
+
+sudo apt install --no-install-recommends -y sddm
