b2a30164e083dea86539924c2b3972a551928b68
iiztp <41050796+iiztp@users.noreply.github.com>
2024-09-13 00:08:15 +0200

    Create install_spicetify.sh

---
+++ Scripts/.debian/install_spicetify.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+#|---/ /+----------------------------------------+---/ /|#
+#|--/ /-| Script to install spicetify            |--/ /-|#
+#|-/ /--| Matthieu Amet                          |-/ /--|#
+#|-/ /--| Prasanth Rangan                        |-/ /--|#
+#|/ /---+----------------------------------------+/ /---|#
+
+curl -fsSL https://raw.githubusercontent.com/spicetify/cli/main/install.sh | sh
+curl -fsSL https://raw.githubusercontent.com/spicetify/marketplace/main/resources/install.sh | sh
