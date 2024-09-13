27712a86e9387557edd941a8bf73512e1074f3e2
Adam Perkowski <adas1per@protonmail.com>
2024-09-13 18:07:31 +0200

    Add bin options

--- Scripts/install.sh
+++ Scripts/install.sh
@@ -113,11 +113,13 @@ EOF
     # get user prefs #
     #----------------#
     if ! chk_list "aurhlpr" "${aurList[@]}"; then
-        echo -e "Available aur helpers:\n[1] yay\n[2] paru"
+        echo -e "Available aur helpers:\n[1] yay\n[2] yay (bin)\n[3] paru\n[4] paru (bin)"
         prompt_timer 120 "Enter option number"
 
         case "${promptIn}" in
             1) export getAur="yay" ;;
+            2) export getAur="yay-bin" ;;
+            1) export getAur="paru" ;;
             2) export getAur="paru-bin" ;;
             *) echo -e "...Invalid option selected..." ; exit 1 ;;
         esac
