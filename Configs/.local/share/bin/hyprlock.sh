60f3c7727d9a9cce535a67bb6d4e8c75e90162c5
Not_Voldemort <46224293+abenezerw@users.noreply.github.com>
2024-09-20 01:45:34 +0800

    Update hyprlock.sh
    No more Spotify mpris because of errors

--- Configs/.local/share/bin/hyprlock.sh
+++ Configs/.local/share/bin/hyprlock.sh
@@ -5,29 +5,8 @@ ScrDir=$(dirname "$(realpath "$0")")
 source ${ScrDir}/globalcontrol.sh
 
 
-mpris_thumb() { # Generate thumbnail for mpris
-    artUrl=$(playerctl -p spotify metadata --format '{{mpris:artUrl}}')
-    [ "${artUrl}" == "$(cat "${THUMB}".lnk)" ] && [ -f "${THUMB}".png ] && exit 0
-    echo "${artUrl}" > "${THUMB}".lnk
-    curl -Lso "${THUMB}".art "$artUrl"
-    convert "${THUMB}.art" -quality 50 "${THUMB}.png"
-    pkill -USR2 hyprlock # updates the mpris thumbnail
-}
-
-fn_mpris() {
-    THUMB="${cacheDir}/landing/mpris"
-    if [ "$(playerctl -p spotify status)" != "Paused" ]; then
-        playerctl -p spotify metadata --format '{{title}}    {{artist}}'  
-        mpris_thumb
-    else :
-        rm -f "${THUMB}".png && exit 0
-    fi
-}
-
 fn_cava() {
-
 :
-
 }
 
 fn_"${1}"
