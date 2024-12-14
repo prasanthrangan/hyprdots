2728954ec6b84767df5681235a8b2e93b48c9368
Anshul Jyoti <81582218+Anshul-007@users.noreply.github.com>
2024-12-15 00:03:06 +0530

    Changing temperaturecontrol script in a easier format
    This will provide user to start sunset mode with turned off functionality, which user has to enable it from waybar and then use it according to his needs everytime he restarts his pc. Also this has 500 steps through which the temperature can be adjusted by that much steps. Also i have added pkill hyprsunset which ensures no more than one hyprsunset instance are running while changing temperature value.

--- Configs/.local/share/bin/temperaturecontrol.sh
+++ Configs/.local/share/bin/temperaturecontrol.sh
@@ -1,38 +1,50 @@
 #!/usr/bin/env sh
+#    ___            __   __          ___       ____       
+#   / _ )___  __ __/ /__/ /__ ____  / _ \___  / / /__ ____
+#  / _  / _ \/ // / / _  / -_) __/ / , _/ _ \/ / / -_) __/
+# /____/\___/\_,_/_/\_,_/\__/_/   /_/|_|\___/_/_/\__/_/   
+#
+# Set directory paths and file locations
+scrDir=$(dirname "$(realpath "$0")")
+source "$scrDir/globalcontrol.sh"
+sunsetConf="/tmp/hyprsunset.json"
 
-# # Causing bottleneck don't use
-# Check if the script is already running
-# if pgrep -cf "${0##*/}" | grep -qv 1; then
-#     echo "An instance of the script is already running..."
-#     exit 1
-# fi
+# Default temperature settings
+default=6500
+step=500
+min=1000
+max=10000
 
-# Check if hyprsunset is running
-HYPRSUNSET_PID=$(pgrep -x hyprsunset)
-# Path to the temperature configuration file
-CONFIG_FILE="$HOME/.config/hypr/hyprsunset.json"
-# notification variable set to true as default with env variable
-notify="${waybar_temperature_notification:-true}" 
-# Default temperature if file is missing
-DEFAULT_TEMP=6500
-# Amount to increase/decrease per scroll
-TEMP_STEP=500  
-# Default action
-action=""
-message=""
-# Minimum and max temp
-MIN_TEMP=1000
-MAX_TEMP=10000
+notify="${waybar_temperature_notification:-true}"
 
-# If a temperature file doesn't exist, create one with default
-if [ ! -f "$CONFIG_FILE" ]; then
-    echo "{\"temp\": $DEFAULT_TEMP, \"user\": 1}" > "$CONFIG_FILE"
+# Ensure the configuration file exists, create it if not
+if [ ! -f "$sunsetConf" ]; then
+    echo "{\"temp\": $default, \"user\": 0}" > "$sunsetConf"
 fi
 
-# Print generic error
-print_error()
-{
-cat << EOF
+# Read current temperature and mode from the configuration file
+currentTemp=$(jq '.temp' "$sunsetConf")
+toggle_mode=$(jq '.user' "$sunsetConf")
+[ -z "$currentTemp" ] && currentTemp=$default
+[ -z "$toggle_mode" ] && toggle_mode=1
+
+# Notification function
+send_notification() {
+    [ -z "$newTemp" ] && newTemp=$default
+    message="Temperature: $newTemp"
+    notify-send -a "t2" -r 91192 -t 800 -i "${confDir}/dunst/icons/therm.svg" "$message"
+}
+
+#keep temp in range
+clamp_temp() {
+    newTemp=$1
+    [ "$newTemp" -lt "$min" ] && newTemp=$min
+    [ "$newTemp" -gt "$max" ] && newTemp=$max
+    echo "$newTemp"
+}
+
+print_error() {
+    cat << EOF
     $(basename ${0}) <action> [mode]
     ...valid actions are...
         i -- <i>ncrease screen temperature [+500]
@@ -46,131 +58,53 @@ cat << EOF
         $(basename ${0}) t -q    # Toggle mode quietly
 EOF
 }
+case "$1" in
+    i) action=increase ;;
+    d) action=decrease ;;
+    r) action=read ;;
+    t) action=toggle ;;
+    *) print_error; exit 1 ;;  # If the argument is invalid, show usage and exit
+esac
 
-send_notification() {
-    ico="$HOME/.config/dunst/icons/therm.svg"
-    notify-send -a "t2" -r 91192 -t 800 -i "${ico}" "$message"
-}
-
-check_range() {
-    # Clamping temperature between valid ranges
-    if [ "$new_temp" -lt "$MIN_TEMP" ]; then
-        new_temp=$MIN_TEMP
-    elif [ "$new_temp" -gt "$MAX_TEMP" ]; then
-        new_temp=$MAX_TEMP
+if [ $# -ge 1 ]; then
+    if [[ "$2" == *q* ]] ; then
+	notify=false
     fi
-    message="Temperature: $new_temp"
-    jq ".temp = $new_temp" "$CONFIG_FILE" > "${CONFIG_FILE}.tmp" && mv "${CONFIG_FILE}.tmp" "$CONFIG_FILE"
-    
-}
-
-# Adjust temperature based on input
-for arg in "$@"; do
-    case $arg in
-        i|-i)   
-            [ -n "$action" ] && 
-                { 
-                    echo -e "\033[38;2;255;0;0mOne or more actions are provided\033[0m"; 
-                    print_error; 
-                    exit 1; 
-                }                       # Prevent multiple actions
-            action="increase" ;;        # Increase temperature
-        d|-d)  
-            [ -n "$action" ] && 
-                { 
-                    echo -e "\033[38;2;255;0;0mOne or more actions are provided\033[0m"; 
-                    print_error; 
-                    exit 1; 
-                }                       # Prevent multiple actions
-            action="decrease" ;;        # Decrease temperature
-        r|-r) 
-            [ -n "$action" ] && 
-                { 
-                    echo -e "\033[38;2;255;0;0mOne or more actions are provided\033[0m"; 
-                    print_error; 
-                    exit 1; 
-                }                       # Prevent multiple actions
-            action="read" ;;            # Read temperature
-        t|-t) 
-            [ -n "$action" ] && 
-                { 
-                    echo -e "\033[38;2;255;0;0mOne or more actions are provided\033[0m"; 
-                    print_error; 
-                    exit 1; 
-                }                       # Prevent multiple actions
-            action="toggle" ;;          # Toggle mode
-        q|-q)  
-            notify=false ;;             # Disabling notification
-        [0-9]*)
-            TEMP_STEP=$arg ;;           # Taking numerical step
-        *)        
-            print_error && exit 1 ;;    # Invalid input
-    esac
-done
-
-# Read current temperature
-current_temp=$(jq '.temp' "$CONFIG_FILE")
-[ -z "$current_temp" ] && current_temp=$DEFAULT_TEMP
-toggle_mode=$(jq '.user' "$CONFIG_FILE")
-[ -z "$toggle_mode" ] && toggle_mode=0
-
-# If actions are missing with mode
-if [ -z "$action" ]; then               
-    echo -e "\033[38;2;255;0;0mActions are not provided\033[0m"
-    print_error
-    exit 1
 fi
 
+# Apply action based on the selected option
 case $action in
-    increase) # increase the temperature
-        # if mode is set then update temperature value
-        if [ "$toggle_mode" = "1" ]; then
-            new_temp=$((current_temp + TEMP_STEP))
-            check_range
-        fi
+    increase) 
+        [ "$toggle_mode" -eq 1 ] && newTemp=$((currentTemp + step)) && newTemp=$(clamp_temp "$newTemp") && echo "{\"temp\": $newTemp, \"user\": $toggle_mode}" > "$sunsetConf"
         ;;
-    decrease) # decrease the temperature
-        # if mode is set then update temperature value
-        if [ "$toggle_mode" = "1" ]; then
-            new_temp=$((current_temp - TEMP_STEP)) 
-            check_range
-        fi
+    decrease) 
+        [ "$toggle_mode" -eq 1 ] && newTemp=$((currentTemp - step)) && newTemp=$(clamp_temp "$newTemp") && echo "{\"temp\": $newTemp, \"user\": $toggle_mode}" > "$sunsetConf"
         ;;
-    read) # read the temperature
-        new_temp=$current_temp
-        message="Temperature: $new_temp"
+    read) 
+        newTemp=$currentTemp
         ;;
-    toggle) # toggle the temperature mode
-        if [ "$toggle_mode" = "1" ]; then
-            new_temp=$DEFAULT_TEMP
-            message="Sunset mode off"
+    toggle) 
+        if [ "$toggle_mode" -eq 1 ]; then
+            newTemp=$default
+            jq '.user = 0' "$sunsetConf" > "${sunsetConf}.tmp" && mv "${sunsetConf}.tmp" "$sunsetConf"
         else
-            new_temp=$current_temp
-            message="Temperature: $new_temp"
+            newTemp=$currentTemp
+            jq '.user = 1' "$sunsetConf" > "${sunsetConf}.tmp" && mv "${sunsetConf}.tmp" "$sunsetConf"
         fi
-        new_toggle_mode=$((1 - toggle_mode))
-        # Setting/Unsetting user flag
-        jq ".user = $new_toggle_mode" "$CONFIG_FILE" > "${CONFIG_FILE}.tmp" && mv "${CONFIG_FILE}.tmp" "$CONFIG_FILE" 
         ;;
 esac
 
-if [ "$toggle_mode" = "1" ]; then
-    echo "{\"alt\":\"active\",\"tooltip\":\"Sunset mode active\"}"
-else
-    echo "{\"alt\":\"inactive\",\"tooltip\":\"Sunset mode inactive\"}"
-fi
-
-# Send notification only when quiet flag is not set
 [ "$notify" = true ] && send_notification
 
+# Start hyprsunset if necessary
 if [ ! "$action" = "read" ]; then
-    # If hyprsunset is running, kill the previous instance
-    if [ -n "$HYPRSUNSET_PID" ]; then
-        kill -9 $HYPRSUNSET_PID 2>/dev/null  # Used -9 for a more forceful termination
-    fi
-
-    # Start hyprsunset with the new temperature if it's not running
+    pkill hyprsunset
     if ! pgrep -x hyprsunset > /dev/null; then
-        hyprsunset --temperature "$new_temp" > /dev/null &
+        hyprsunset --temperature "$newTemp" > /dev/null &
     fi
 fi
+
+# Print status message
+
+echo "{\"alt\":\"$( [ "$toggle_mode" -eq 1 ] && echo 'active' || echo 'inactive' )\", \"tooltip\":\"Sunset mode $( [ "$toggle_mode" -eq 1 ] && echo 'active' || echo 'inactive' )\"}"
+
